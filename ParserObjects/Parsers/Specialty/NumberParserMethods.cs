17864bd5b1bd9f79f39e60ad3b84f98bcccb67f4
Andrew F. Whitworth <awhitworth@asicentral.com>
2020-01-11 06:53:10 -0500

    prototype of possible parser caching mechanism

--- ParserObjects/Parsers/Specialty/NumberParserMethods.cs
+++ ParserObjects/Parsers/Specialty/NumberParserMethods.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using static ParserObjects.Parsers.ParserMethods;
 
 namespace ParserObjects.Parsers.Specialty
@@ -8,13 +8,26 @@ namespace ParserObjects.Parsers.Specialty
     {
         // TODO: Parsers which are invariant should be cached for performance
 
-        public static IParser<char, char> Digit() => Match<char>(char.IsDigit);
-        public static IParser<char, char> NonZeroDigit() => Match<char>(c =>  c != '0' && char.IsDigit(c));
+        public static IParser<char, char> Digit() 
+            => ParserCache.Instance.GetParser(nameof(Digit), () => Match<char>(char.IsDigit));
 
-        public static IParser<char, string> DigitString() => Digit().List(d => new string(d.ToArray()), true);
+        private static IParser<char, char> NonZeroDigitInternal()
+            => Match<char>(c => c != '0' && char.IsDigit(c));
+        public static IParser<char, char> NonZeroDigit() 
+            => ParserCache.Instance.GetParser(nameof(NonZeroDigit), NonZeroDigitInternal);
+
+        public static IParser<char, string> DigitString() 
+            => ParserCache.Instance.GetParser(nameof(DigitString), () => Digit().ListCharToString(true));
 
         private static readonly HashSet<char> _hexDigits = new HashSet<char>("abcdefABCDEF0123456789");
-        public static IParser<char, char> HexadecimalDigit() => Match<char>(c => _hexDigits.Contains(c));
-        public static IParser<char, string> HexadecimalString() => HexadecimalDigit().List(x => new string(x.ToArray()));
+        private static IParser<char, char> HexadecimalDigitInternal() 
+            => Match<char>(c => _hexDigits.Contains(c));
+        public static IParser<char, char> HexadecimalDigit() 
+            => ParserCache.Instance.GetParser(nameof(HexadecimalDigit), HexadecimalDigitInternal);
+
+        private static IParser<char, string> HexadecimalStringInternal() 
+            => HexadecimalDigit().ListCharToString();
+        public static IParser<char, string> HexadecimalString() 
+            => ParserCache.Instance.GetParser(nameof(HexadecimalString), HexadecimalStringInternal);
     }
 }
