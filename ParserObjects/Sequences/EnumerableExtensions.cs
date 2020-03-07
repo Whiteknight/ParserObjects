eb1bd7613223f2dac5c8d476afe6cc234df3af47
Andrew F. Whitworth <awhitworth@asicentral.com>
2020-03-07 14:25:12 -0500

    Some misc cleanups and docs

--- ParserObjects/Sequences/EnumerableExtensions.cs
+++ ParserObjects/Sequences/EnumerableExtensions.cs
@@ -5,9 +5,23 @@ namespace ParserObjects.Sequences
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Wrap the enumerable as a sequence.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <param name="endValue">An end value to return when the sequence is exhausted</param>
+        /// <returns></returns>
         public static ISequence<T> AsSequence<T>(this IEnumerable<T> enumerable, T endValue = default)
             => new EnumerableSequence<T>(enumerable, endValue);
 
+        /// <summary>
+        /// Wrap the enumerable as a sequence
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <param name="getEndValue">Get a value to return when the sequence is exhausted</param>
+        /// <returns></returns>
         public static ISequence<T> AsSequence<T>(this IEnumerable<T> enumerable, Func<T> getEndValue)
             => new EnumerableSequence<T>(enumerable, getEndValue);
     }
--- ParserObjects/Sequences/StringExtensions.cs
+++ ParserObjects/Sequences/StringExtensions.cs
@@ -2,6 +2,11 @@
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Wrap the string as a sequence of characters
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public static ISequence<char> AsCharacterSequence(this string str)
             => new StringCharacterSequence(str);
     }
