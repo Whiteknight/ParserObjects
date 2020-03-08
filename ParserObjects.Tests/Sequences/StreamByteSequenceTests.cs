3d60c77eeec2714e5ae578397b9f7026fe080dac
Andrew F. Whitworth <awhitworth@asicentral.com>
2020-03-07 21:30:29 -0500

    ISequence<byte> for streams

---
+++ ParserObjects.Tests/Sequences/StreamByteSequenceTests.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using ParserObjects.Sequences;
+
+namespace ParserObjects.Tests.Sequences
+{
+    public class StreamByteSequenceTests
+    {
+        private static StreamByteSequence GetTarget(params byte[] b)
+        {
+            var memoryStream = new MemoryStream();
+            memoryStream.Write(b);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            return new StreamByteSequence(memoryStream);
+        }
+
+        [Test]
+        public void GetNext_Test()
+        {
+            var target = GetTarget(1, 2, 3);
+            target.GetNext().Should().Be(1);
+            target.GetNext().Should().Be(2);
+            target.GetNext().Should().Be(3);
+            target.GetNext().Should().Be(0);
+        }
+
+        [Test]
+        public void Peek_Test()
+        {
+            var target = GetTarget(1, 2, 3);
+            target.Peek().Should().Be(1);
+            target.Peek().Should().Be(1);
+            target.Peek().Should().Be(1);
+        }
+
+        [Test]
+        public void GetNextPutBack_Test()
+        {
+            var target = GetTarget(1, 3);
+            target.GetNext().Should().Be(1);
+            target.PutBack(2);
+            target.GetNext().Should().Be(2);
+            target.GetNext().Should().Be(3);
+            target.GetNext().Should().Be(0);
+        }
+
+        [Test]
+        public void PutBack_Test()
+        {
+            var target = GetTarget(1, 2, 3);
+            target.GetNext().Should().Be(1);
+            target.PutBack(1);
+            target.GetNext().Should().Be(1);
+            target.GetNext().Should().Be(2);
+            target.PutBack(2);
+            target.GetNext().Should().Be(2);
+            target.GetNext().Should().Be(3);
+            target.GetNext().Should().Be(0);
+        }
+    }
+}
---
+++ ParserObjects/Sequences/StreamByteSequence.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ParserObjects.Sequences
+{
+    public class StreamByteSequence : ISequence<byte>, IDisposable
+    {
+        private const int BufferSize = 128;
+
+        private readonly string _fileName;
+        private readonly Stream _stream;
+        private readonly Stack<byte> _putbacks;
+        private readonly byte[] _buffer;
+
+        private bool _isComplete;
+        private int _remainingBytes;
+        private int _bufferIndex;
+        private int _index;
+
+        public StreamByteSequence(string fileName)
+        {
+            _fileName = fileName;
+            _putbacks = new Stack<byte>();
+            _bufferIndex = BufferSize;
+            _buffer = new byte[BufferSize];
+            _stream = File.OpenRead(_fileName);
+        }
+
+        public StreamByteSequence(Stream stream, string fileName = null)
+        {
+            _fileName = fileName;
+            _putbacks = new Stack<byte>();
+            _bufferIndex = BufferSize;
+            _buffer = new byte[BufferSize];
+            _stream = stream;
+        }
+
+        public byte GetNext()
+        {
+            var b = GetNextByteRaw(true);
+            _index++;
+            return b;
+        }
+
+        public void PutBack(byte value)
+        {
+            _putbacks.Push(value);
+        }
+
+        public byte Peek() => GetNextByteRaw(false);
+
+        public Location CurrentLocation => new Location(_fileName, 0, _index);
+
+        public bool IsAtEnd => _putbacks.Count == 0 && _isComplete;
+
+        public void Dispose()
+        {
+            _stream?.Dispose();
+        }
+
+        private byte GetNextByteRaw(bool advance)
+        {
+            if (_putbacks.Count > 0)
+                return advance ? _putbacks.Pop() : _putbacks.Peek();
+            if (_isComplete)
+                return 0;
+            FillBuffer();
+            if (_isComplete || _remainingBytes == 0 || _bufferIndex >= BufferSize)
+                return 0;
+            var b = _buffer[_bufferIndex];
+            if (advance)
+            {
+                _bufferIndex++;
+                _remainingBytes--;
+            }
+            return b;
+        }
+
+        private void FillBuffer()
+        {
+            if (_remainingBytes != 0 && _bufferIndex < BufferSize)
+                return;
+            _remainingBytes = _stream.Read(_buffer, 0, BufferSize);
+            if (_remainingBytes == 0)
+                _isComplete = true;
+            _bufferIndex = 0;
+        }
+    }
+}
