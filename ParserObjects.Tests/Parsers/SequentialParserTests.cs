c18af41d7db41e7b53611a823c1da8a630e57023
Whiteknight <wknight8111@gmail.com>
2020-11-11 19:29:35 -0500

    Add a SequentialParser which allows us to parse parsers sequentially
    Pro: Can visualize logic procedurally
    Pro: Can set breakpoints and see exactly what is happening
    Con: Cannot use ReplaceChild
    Con: Cannot get good BNF stringification

---
+++ ParserObjects.Tests/Parsers/SequentialParserTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using static ParserObjects.ParserMethods<char>;
+
+namespace ParserObjects.Tests.Parsers
+{
+    public class SequentialParserTests
+    {
+        [Test]
+        public void Parse_Test()
+        {
+            var parser = Sequential(s =>
+            {
+                var first = s.Parse(Any());
+                char second;
+                if (first == 'a')
+                    second = s.Parse(Match('b'));
+                else
+                    second = s.Parse(Match('y'));
+                var third = s.Parse(Match('c'));
+
+                return $"{first}{second}{third}".ToUpper();
+            });
+
+            var result = parser.Parse("abc");
+            result.Success.Should().BeTrue();
+            result.Value.Should().Be("ABC");
+
+            result = parser.Parse("xyc");
+            result.Success.Should().BeTrue();
+            result.Value.Should().Be("XYC");
+        }
+    }
+}
---
+++ ParserObjects/Parsers/SequentialParser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParserObjects.Parsers
+{
+    public class SequentialState<TInput>
+    {
+        private readonly ISequence<TInput> _input;
+
+        public SequentialState(ISequence<TInput> input)
+        {
+            _input = input;
+        }
+
+        public TValue Parse<TValue>(IParser<TInput, TValue> p)
+        {
+            var result = p.Parse(_input);
+            if (!result.Success)
+                throw new SequentialParserException(result.Location);
+            return result.Value;
+        }
+    }
+
+    public class SequentialParserException : Exception
+    {
+        public SequentialParserException(Location location)
+        {
+            Location = location;
+        }
+
+        public Location Location { get; }
+    }
+
+    public class SequentialParser<TInput, TOutput> : IParser<TInput, TOutput>
+    {
+        private readonly Func<SequentialState<TInput>, TOutput> _func;
+
+        public SequentialParser(Func<SequentialState<TInput>, TOutput> func)
+        {
+            _func = func;
+        }
+
+        public string Name { get; set; }
+
+        public IResult<TOutput> Parse(ISequence<TInput> t)
+        {
+            var startLocation = t.CurrentLocation;
+            var window = t.Window();
+            try
+            {
+                var state = new SequentialState<TInput>(window);
+                var result = _func(state);
+                return Result.Success(result, startLocation);
+            }
+            catch (SequentialParserException spe)
+            {
+                // This exception is part of normal flow-control for this parser
+                // Other exceptions bubble up like normal.
+                window.Rewind();
+                return Result.Fail<TOutput>(spe.Location);
+            }
+        }
+
+        public IResult<object> ParseUntyped(ISequence<TInput> t) => Parse(t).Untype();
+
+        public IEnumerable<IParser> GetChildren() => Enumerable.Empty<IParser>();
+
+        // TODO: It would be trivial to add a hashmap oldParser->newParser and do the replacement
+        // in State.Parse(). Is it worth it?
+        public IParser ReplaceChild(IParser find, IParser replace) => this;
+    }
+}
