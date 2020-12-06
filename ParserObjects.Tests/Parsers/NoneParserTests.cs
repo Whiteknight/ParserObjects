88b801b8bb6d7bbfb0891485e0e2077c1dedfa16
Whiteknight <wknight8111@gmail.com>
2020-12-06 17:55:52 -0500

    Improve test coverage for Sequential and None parsers

---
+++ ParserObjects.Tests/Parsers/NoneParserTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ParserObjects.Sequences;
+using static ParserObjects.ParserMethods<char>;
+
+namespace ParserObjects.Tests.Parsers
+{
+    public class NoneParserTests
+    {
+        [Test]
+        public void Parse_Output_Test()
+        {
+            var target = Any().None();
+            var input = new StringCharacterSequence("abc");
+            var result = target.Parse(input);
+            result.Success.Should().BeTrue();
+            result.Value.Should().Be('a');
+            input.Peek().Should().Be('a');
+        }
+
+        [Test]
+        public void Parse_Output_Fail()
+        {
+            var target = Fail<char>().None();
+            var input = new StringCharacterSequence("abc");
+            var result = target.Parse(input);
+            result.Success.Should().BeFalse();
+            input.Peek().Should().Be('a');
+        }
+
+        [Test]
+        public void Parse_Test()
+        {
+            var target = And(Any(), Any()).None();
+            var input = new StringCharacterSequence("abc");
+            var result = target.Parse(input);
+            result.Success.Should().BeTrue();
+            input.Peek().Should().Be('a');
+        }
+
+        [Test]
+        public void Parse_Fail()
+        {
+            var target = And(Any(), Fail<char>()).None();
+            var input = new StringCharacterSequence("abc");
+            var result = target.Parse(input);
+            result.Success.Should().BeFalse();
+            input.Peek().Should().Be('a');
+        }
+    }
+}
--- ParserObjects.Tests/Parsers/SequentialParserTests.cs
+++ ParserObjects.Tests/Parsers/SequentialParserTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using ParserObjects.Sequences;
 using static ParserObjects.ParserMethods<char>;
 
 namespace ParserObjects.Tests.Parsers
@@ -29,6 +30,53 @@ namespace ParserObjects.Tests.Parsers
             result = parser.Parse("xyc");
             result.Success.Should().BeTrue();
             result.Value.Should().Be("XYC");
+        }
+
+        [Test]
+        public void Parse_Fail()
+        {
+            var parser = Sequential<object>(s =>
+            {
+                s.Parse(Fail<bool>());
+                return null;
+            });
+
+            var result = parser.Parse("abc");
+            result.Success.Should().BeFalse();
+        }
+
+        [Test]
+        public void Parse_TryParse()
+        {
+            var parser = Sequential(s =>
+            {
+                var first = s.TryParse(Any());
+                var second = s.TryParse(Fail<bool>());
+                return first.Value;
+            });
+
+            var result = parser.Parse("abc");
+            result.Success.Should().BeTrue();
+            result.Value.Should().Be('a');
+        }
+
+        [Test]
+        public void Parse_Fail_Rewind()
+        {
+            var parser = Sequential(s =>
+            {
+                var first = s.Parse(Any());
+                var second = s.Parse(Any());
+                var third = s.Parse(Any());
+                var fail = s.Parse(Fail<char>());
+
+                return $"{first}{second}{third}".ToUpper();
+            });
+
+            var input = new StringCharacterSequence("abc");
+            var result = parser.Parse("abc");
+            result.Success.Should().BeFalse();
+            input.Peek().Should().Be('a');
         }
     }
 }
