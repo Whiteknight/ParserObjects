78f94178b10bbc4e163002951625840f37ddfe1a
Whiteknight <wknight8111@gmail.com>
2021-01-06 19:02:20 -0500

    Add a unique ID generator that we can use later to generate ID and Name values

---
+++ ParserObjects/Utility/UniqueIntegerGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+
+namespace ParserObjects.Utility
+{
+    public static class UniqueIntegerGenerator
+    {
+        private static int _value;
+
+        public static int GetNext() => Interlocked.Increment(ref _value);
+    }
+}
