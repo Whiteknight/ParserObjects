986589b5251cd7ba40fbc10b5eac50e134879b31
Whiteknight <wknight8111@gmail.com>
2021-11-18 01:27:43 -0500

    A few more tests for DataContext multi

--- ParserObjects.Tests/Parsers/StateParserTests.cs
+++ ParserObjects.Tests/Parsers/StateParserTests.cs
@@ -8,7 +8,7 @@ namespace ParserObjects.Tests.Parsers
     public class StateParserTests
     {
         [Test]
-        public void StateData_Test1()
+        public void StateData_SingleOutput_Test1()
         {
             var target = DataContext(
                 Rule(
@@ -24,7 +24,7 @@ namespace ParserObjects.Tests.Parsers
         }
 
         [Test]
-        public void StateData_Test2()
+        public void StateData_SingleOutput_Test2()
         {
             var target = DataContext(
                 Rule(
@@ -45,7 +45,7 @@ namespace ParserObjects.Tests.Parsers
         }
 
         [Test]
-        public void StateData_Test3()
+        public void StateData_SingleOutput_Test3()
         {
             var target = DataContext(
                 GetData<string>("a"),
@@ -55,6 +55,63 @@ namespace ParserObjects.Tests.Parsers
             var result = target.Parse("");
             result.Success.Should().BeTrue();
             result.Value.Should().Be("1");
+        }
+
+        [Test]
+        public void StateData_MultiOutput_NameValue()
+        {
+            var target = DataContext(
+                ProduceMulti((i, d) =>
+                {
+                    var x = d.Get<char>("A").Value;
+                    return new[] { x };
+                }), "A", 'X'
+            );
+
+            var result = target.Parse("");
+            result.Success.Should().BeTrue();
+            result.Results[0].Value.Should().Be('X');
+        }
+
+        [Test]
+        public void StateData_MultiOutput_Dictionary()
+        {
+            var target = DataContext(
+                ProduceMulti((i, d) =>
+                {
+                    return new[] { d.Get<char>("A").Value, d.Get<char>("B").Value };
+                }),
+                new Dictionary<string, char>
+                {
+                    { "A", 'X' },
+                    { "B", 'Y' }
+                }
+            );
+
+            var result = target.Parse("");
+            result.Success.Should().BeTrue();
+            result.Results.Count.Should().Be(2);
+            result.Results[0].Value.Should().Be('X');
+            result.Results[1].Value.Should().Be('Y');
+        }
+
+        [Test]
+        public void StateData_MultiOutput()
+        {
+            var target = DataContext(
+                ProduceMulti((i, d) =>
+                {
+                    d.Set("A", 'X');
+                    d.Set("B", 'Y');
+                    return new[] { d.Get<char>("A").Value, d.Get<char>("B").Value };
+                })
+            );
+
+            var result = target.Parse("");
+            result.Success.Should().BeTrue();
+            result.Results.Count.Should().Be(2);
+            result.Results[0].Value.Should().Be('X');
+            result.Results[1].Value.Should().Be('Y');
         }
     }
 }
