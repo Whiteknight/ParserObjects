cc9f6a24b2eab77deffbb5afd1fd33b5031b0336
Whiteknight <wknight8111@gmail.com>
2021-11-18 02:04:17 -0500

    v3.1.0-beta1

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ParserObjects/bin/Release
-dotnet nuget push ParserObjects.$1.nupkg --source https://www.nuget.org/api/v2/package
-dotnet nuget push ParserObjects.$1.snupkg --source https://www.nuget.org/api/v2/package
+nuget push ParserObjects.$1.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push ParserObjects.$1.snupkg -Source https://www.nuget.org/api/v2/package
