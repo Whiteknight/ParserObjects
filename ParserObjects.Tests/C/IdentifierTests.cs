9f5a1d044d8aabd4ff4d7a3cbe0d7b8f29c02639
Whiteknight <wknight8111@gmail.com>
2022-11-12 09:44:54 -0500

    Expand C-style Identifier tests

--- ParserObjects.Tests/C/IdentifierTests.cs
+++ ParserObjects.Tests/C/IdentifierTests.cs
@@ -4,16 +4,19 @@ namespace ParserObjects.Tests.C;
 
 internal class IdentifierTests
 {
-    [Test]
-    public void CStyleIdentifier_Tests()
+    [TestCase("_", true)]
+    [TestCase("a", true)]
+    [TestCase("_a", true)]
+    [TestCase("_a_", true)]
+    [TestCase("a1", true)]
+    [TestCase("_1", true)]
+    [TestCase("_1_", true)]
+    [TestCase("test", true)]
+    [TestCase("0", false)]
+    [TestCase("-a", false)]
+    public void Parse_Test(string test, bool shouldMatch)
     {
         var parser = Identifier();
-        parser.CanMatch("_").Should().BeTrue();
-        parser.CanMatch("a").Should().BeTrue();
-        parser.CanMatch("a1").Should().BeTrue();
-        parser.CanMatch("_1").Should().BeTrue();
-        parser.CanMatch("test").Should().BeTrue();
-        parser.CanMatch("0").Should().BeFalse();
-        parser.CanMatch("-a").Should().BeFalse();
+        parser.CanMatch(test).Should().Be(shouldMatch);
     }
 }
