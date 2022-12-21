ffefd8b17cbdd2d4012fbdd52a2f1e3ad9492512
Whiteknight <wknight8111@gmail.com>
2022-12-21 12:26:52 -0500

    Tests for Not()

--- ParserObjects.Tests/Parsers/NotParserTests.cs
+++ ParserObjects.Tests/Parsers/NotParserTests.cs
@@ -3,41 +3,102 @@ using static ParserObjects.Sequences;
 
 namespace ParserObjects.Tests.Parsers;
 
-public class NotParserTests
+public static class NotParserTests
 {
-    private readonly IParser<char, char> _anyParser = Any();
-    private readonly IParser<char, bool> _failParser = Fail<bool>();
+    private static readonly IParser<char, char> _anyParser = Any();
+    private static readonly IParser<char, bool> _failParser = Fail<bool>();
 
-    [Test]
-    public void Parse_Fail()
+    public class Method
     {
-        var parser = Not(_failParser);
+        [Test]
+        public void Parse_Fail()
+        {
+            var parser = Not(_failParser);
 
-        var input = FromString("abc");
-        var result = parser.Parse(input);
-        result.Success.Should().BeTrue();
-        result.Consumed.Should().Be(0);
+            var input = FromString("abc");
+            var result = parser.Parse(input);
+            result.Success.Should().BeTrue();
+            result.Consumed.Should().Be(0);
+        }
+
+        [Test]
+        public void Parse_Success()
+        {
+            var parser = Not(_anyParser);
+
+            var input = FromString("abc");
+            var result = parser.Parse(input);
+            result.Success.Should().BeFalse();
+            result.Consumed.Should().Be(0);
+        }
+
+        [Test]
+        public void Match_Fail()
+        {
+            var parser = Not(_failParser);
+
+            var input = FromString("abc");
+            var result = parser.Match(input);
+            result.Should().BeTrue();
+            input.Consumed.Should().Be(0);
+        }
+
+        [Test]
+        public void Match_Success()
+        {
+            var parser = Not(_anyParser);
+
+            var input = FromString("abc");
+            var result = parser.Match(input);
+            result.Should().BeFalse();
+            input.Consumed.Should().Be(0);
+        }
     }
 
-    [Test]
-    public void NotExtension_Parse_Fail()
+    public class Extension
     {
-        var parser = _failParser.Not();
+        [Test]
+        public void Parse_Fail()
+        {
+            var parser = _failParser.Not();
 
-        var input = FromString("abc");
-        var result = parser.Parse(input);
-        result.Success.Should().BeTrue();
-        result.Consumed.Should().Be(0);
-    }
+            var input = FromString("abc");
+            var result = parser.Parse(input);
+            result.Success.Should().BeTrue();
+            result.Consumed.Should().Be(0);
+        }
 
-    [Test]
-    public void Parse_Success()
-    {
-        var parser = Not(_anyParser);
+        [Test]
+        public void Parse_Success()
+        {
+            var parser = _anyParser.Not();
 
-        var input = FromString("abc");
-        var result = parser.Parse(input);
-        result.Success.Should().BeFalse();
-        result.Consumed.Should().Be(0);
+            var input = FromString("abc");
+            var result = parser.Parse(input);
+            result.Success.Should().BeFalse();
+            result.Consumed.Should().Be(0);
+        }
+
+        [Test]
+        public void Match_Fail()
+        {
+            var parser = _failParser.Not();
+
+            var input = FromString("abc");
+            var result = parser.Match(input);
+            result.Should().BeTrue();
+            input.Consumed.Should().Be(0);
+        }
+
+        [Test]
+        public void Match_Success()
+        {
+            var parser = _anyParser.Not();
+
+            var input = FromString("abc");
+            var result = parser.Match(input);
+            result.Should().BeFalse();
+            input.Consumed.Should().Be(0);
+        }
     }
 }
