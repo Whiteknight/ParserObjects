b7000e64a8fd41a61482392f3b30b737ba601d9b
Whiteknight <wknight8111@gmail.com>
2022-12-21 10:39:21 -0500

    A bit more test coverage for Cache() parser

--- ParserObjects.Tests/Parsers/CacheParserTests.cs
+++ ParserObjects.Tests/Parsers/CacheParserTests.cs
@@ -5,121 +5,230 @@ using static ParserObjects.Sequences;
 
 namespace ParserObjects.Tests.Parsers
 {
-    public class CacheParserTests
+    public static class CacheTests
     {
-        [Test]
-        public void Method_Parse_NoOutput()
+        public class MethodNoOutput
         {
-            var parser = Cache(End());
-            var input = FromString("TEST");
-            var start = input.Checkpoint();
-            var cache = InMemoryCache();
-            var state = new ParseState<char>(input, _ => { }, cache);
+            [Test]
+            public void Parse_Test()
+            {
+                var parser = Cache(End());
+                var input = FromString("TEST");
+                var start = input.Checkpoint();
+                var cache = InMemoryCache();
+                var state = new ParseState<char>(input, _ => { }, cache);
 
-            // First attempt, we parse the string for the first time. There's nothing in cache
-            var result = parser.Parse(state);
-            result.Success.Should().BeFalse();
-            var stats = cache.GetStatistics();
-            stats.Attempts.Should().Be(1);
-            stats.Misses.Should().Be(1);
-            stats.Hits.Should().Be(0);
+                // First attempt, we parse the string for the first time. There's nothing in cache
+                var result = parser.Parse(state);
+                result.Success.Should().BeFalse();
+                var stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(1);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(0);
 
-            // Second attempt, we start from the beginning again, but now the value is in cache
-            // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
-            // the new hit
-            start.Rewind();
-            result = parser.Parse(state);
-            result.Success.Should().BeFalse();
-            stats = cache.GetStatistics();
-            stats.Attempts.Should().Be(2);
-            stats.Misses.Should().Be(1);
-            stats.Hits.Should().Be(1);
+                // Second attempt, we start from the beginning again, but now the value is in cache
+                // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
+                // the new hit
+                start.Rewind();
+                result = parser.Parse(state);
+                result.Success.Should().BeFalse();
+                stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(2);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(1);
+            }
+
+            [Test]
+            public void ToBnf_Test()
+            {
+                var parser = Cache(End());
+                var result = parser.ToBnf();
+                result.Should().Contain("(TARGET) := CACHED(END)");
+            }
         }
 
-        [Test]
-        public void Method_Parse_Output()
+        public class MethodSingle
         {
-            var parser = Cache(CharacterString("TEST"));
-            var input = FromString("TEST");
-            var start = input.Checkpoint();
-            var cache = InMemoryCache();
-            var state = new ParseState<char>(input, _ => { }, cache);
+            [Test]
+            public void Parse_Test()
+            {
+                var parser = Cache(CharacterString("TEST"));
+                var input = FromString("TEST");
+                var start = input.Checkpoint();
+                var cache = InMemoryCache();
+                var state = new ParseState<char>(input, _ => { }, cache);
 
-            // First attempt, we parse the string for the first time. There's nothing in cache
-            var result = parser.Parse(state);
-            result.Success.Should().BeTrue();
-            result.Value.Should().Be("TEST");
-            var stats = cache.GetStatistics();
-            stats.Attempts.Should().Be(1);
-            stats.Misses.Should().Be(1);
-            stats.Hits.Should().Be(0);
+                // First attempt, we parse the string for the first time. There's nothing in cache
+                var result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Value.Should().Be("TEST");
+                var stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(1);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(0);
 
-            // Second attempt, we start from the beginning again, but now the value is in cache
-            // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
-            // the new hit
-            start.Rewind();
-            result = parser.Parse(state);
-            result.Success.Should().BeTrue();
-            result.Value.Should().Be("TEST");
-            stats = cache.GetStatistics();
-            stats.Attempts.Should().Be(2);
-            stats.Misses.Should().Be(1);
-            stats.Hits.Should().Be(1);
+                // Second attempt, we start from the beginning again, but now the value is in cache
+                // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
+                // the new hit
+                start.Rewind();
+                result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Value.Should().Be("TEST");
+                stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(2);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(1);
+            }
+
+            [Test]
+            public void ToBnf_Test()
+            {
+                var parser = Cache(CharacterString("TEST"));
+                var result = parser.ToBnf();
+                result.Should().Contain("(TARGET) := CACHED('T' 'E' 'S' 'T')");
+            }
         }
 
-        [Test]
-        public void Method_Parse_MultiOutput()
+        public class MethodMulti
         {
-            var parser = Cache(ProduceMulti(() => new[] { "abc" }));
-            var input = FromString("TEST");
-            var start = input.Checkpoint();
-            var cache = InMemoryCache();
-            var state = new ParseState<char>(input, _ => { }, cache);
+            [Test]
+            public void Parse_Test()
+            {
+                var parser = Cache(ProduceMulti(() => new[] { "abc" }));
+                var input = FromString("TEST");
+                var start = input.Checkpoint();
+                var cache = InMemoryCache();
+                var state = new ParseState<char>(input, _ => { }, cache);
 
-            // First attempt, we parse the string for the first time. There's nothing in cache
-            var result = parser.Parse(state);
-            result.Success.Should().BeTrue();
-            result.Results[0].Value.Should().Be("abc");
-            var stats = cache.GetStatistics();
-            stats.Attempts.Should().Be(1);
-            stats.Misses.Should().Be(1);
-            stats.Hits.Should().Be(0);
+                // First attempt, we parse the string for the first time. There's nothing in cache
+                var result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Results[0].Value.Should().Be("abc");
+                var stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(1);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(0);
 
-            // Second attempt, we start from the beginning again, but now the value is in cache
-            // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
-            // the new hit
-            start.Rewind();
-            result = parser.Parse(state);
-            result.Success.Should().BeTrue();
-            result.Results[0].Value.Should().Be("abc");
-            stats = cache.GetStatistics();
-            stats.Attempts.Should().Be(2);
-            stats.Misses.Should().Be(1);
-            stats.Hits.Should().Be(1);
+                // Second attempt, we start from the beginning again, but now the value is in cache
+                // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
+                // the new hit
+                start.Rewind();
+                result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Results[0].Value.Should().Be("abc");
+                stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(2);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(1);
+            }
+
+            [Test]
+            public void ToBnf_Test()
+            {
+                var parser = Cache(ProduceMulti(() => new[] { "abc" }));
+                var result = parser.ToBnf();
+                result.Should().Contain("(TARGET) := CACHED(PRODUCE)");
+            }
         }
 
-        [Test]
-        public void ToBnf_NoValue()
+        public class ExtensionNoOutput
         {
-            var parser = Cache(End());
-            var result = parser.ToBnf();
-            result.Should().Contain("(TARGET) := CACHED(END)");
+            [Test]
+            public void Parse_Test()
+            {
+                var parser = End().Cache();
+                var input = FromString("TEST");
+                var start = input.Checkpoint();
+                var cache = InMemoryCache();
+                var state = new ParseState<char>(input, _ => { }, cache);
+
+                // First attempt, we parse the string for the first time. There's nothing in cache
+                var result = parser.Parse(state);
+                result.Success.Should().BeFalse();
+                var stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(1);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(0);
+
+                // Second attempt, we start from the beginning again, but now the value is in cache
+                // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
+                // the new hit
+                start.Rewind();
+                result = parser.Parse(state);
+                result.Success.Should().BeFalse();
+                stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(2);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(1);
+            }
         }
 
-        [Test]
-        public void ToBnf_SingleValue()
+        public class ExtensionSingle
         {
-            var parser = Cache(CharacterString("TEST"));
-            var result = parser.ToBnf();
-            result.Should().Contain("(TARGET) := CACHED('T' 'E' 'S' 'T')");
+            [Test]
+            public void Parse_Test()
+            {
+                var parser = CharacterString("TEST").Cache();
+                var input = FromString("TEST");
+                var start = input.Checkpoint();
+                var cache = InMemoryCache();
+                var state = new ParseState<char>(input, _ => { }, cache);
+
+                // First attempt, we parse the string for the first time. There's nothing in cache
+                var result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Value.Should().Be("TEST");
+                var stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(1);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(0);
+
+                // Second attempt, we start from the beginning again, but now the value is in cache
+                // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
+                // the new hit
+                start.Rewind();
+                result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Value.Should().Be("TEST");
+                stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(2);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(1);
+            }
         }
 
-        [Test]
-        public void ToBnf_MultiValue()
+        public class ExtensionMulti
         {
-            var parser = Cache(ProduceMulti(() => new[] { "abc" }));
-            var result = parser.ToBnf();
-            result.Should().Contain("(TARGET) := CACHED(PRODUCE)");
+            [Test]
+            public void Parse_Test()
+            {
+                var parser = ProduceMulti(() => new[] { "abc" }).Cache();
+                var input = FromString("TEST");
+                var start = input.Checkpoint();
+                var cache = InMemoryCache();
+                var state = new ParseState<char>(input, _ => { }, cache);
+
+                // First attempt, we parse the string for the first time. There's nothing in cache
+                var result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Results[0].Value.Should().Be("abc");
+                var stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(1);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(0);
+
+                // Second attempt, we start from the beginning again, but now the value is in cache
+                // already. We keep the same statistics from last time (1 attempt, 1 miss) and append
+                // the new hit
+                start.Rewind();
+                result = parser.Parse(state);
+                result.Success.Should().BeTrue();
+                result.Results[0].Value.Should().Be("abc");
+                stats = cache.GetStatistics();
+                stats.Attempts.Should().Be(2);
+                stats.Misses.Should().Be(1);
+                stats.Hits.Should().Be(1);
+            }
         }
     }
 }
