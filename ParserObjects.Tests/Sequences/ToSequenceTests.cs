ea9799f901bd74ba840d37bed4f1fff53b03b9e2
Whiteknight <wknight8111@gmail.com>
2022-12-21 11:46:55 -0500

    Tests for getting a sequence from IEnumerable
    Previous tests were only for the IReadOnlyList special-case

--- ParserObjects.Tests/Sequences/ToSequenceTests.cs
+++ ParserObjects.Tests/Sequences/ToSequenceTests.cs
@@ -1,24 +1,52 @@
-﻿namespace ParserObjects.Tests.Sequences;
+﻿using System.Linq;
 
-internal class ToSequenceTests
+namespace ParserObjects.Tests.Sequences;
+
+public static class ToSequenceTests
 {
-    [Test]
-    public void AsSequence_GetNext_Test()
+    public class ReadOnlyList
     {
-        var target = new[] { 1, 2, 3 }.ToSequence();
-        target.GetNext().Should().Be(1);
-        target.GetNext().Should().Be(2);
-        target.GetNext().Should().Be(3);
-        target.GetNext().Should().Be(0);
+        [Test]
+        public void AsSequence_GetNext_Test()
+        {
+            var target = new[] { 1, 2, 3 }.ToSequence();
+            target.GetNext().Should().Be(1);
+            target.GetNext().Should().Be(2);
+            target.GetNext().Should().Be(3);
+            target.GetNext().Should().Be(0);
+        }
+
+        [Test]
+        public void AsSequence_GetNext_EndValue()
+        {
+            var target = new[] { 1, 2, 3 }.ToSequence(100);
+            target.GetNext().Should().Be(1);
+            target.GetNext().Should().Be(2);
+            target.GetNext().Should().Be(3);
+            target.GetNext().Should().Be(100);
+        }
     }
 
-    [Test]
-    public void AsSequence_GetNext_EndValue()
+    public class Enumerable
     {
-        var target = new[] { 1, 2, 3 }.ToSequence(100);
-        target.GetNext().Should().Be(1);
-        target.GetNext().Should().Be(2);
-        target.GetNext().Should().Be(3);
-        target.GetNext().Should().Be(100);
+        [Test]
+        public void AsSequence_GetNext_Test()
+        {
+            var target = new[] { 1, 2, 3 }.Select(x => x).ToSequence();
+            target.GetNext().Should().Be(1);
+            target.GetNext().Should().Be(2);
+            target.GetNext().Should().Be(3);
+            target.GetNext().Should().Be(0);
+        }
+
+        [Test]
+        public void AsSequence_GetNext_EndValue()
+        {
+            var target = new[] { 1, 2, 3 }.Select(x => x).ToSequence(100);
+            target.GetNext().Should().Be(1);
+            target.GetNext().Should().Be(2);
+            target.GetNext().Should().Be(3);
+            target.GetNext().Should().Be(100);
+        }
     }
 }
