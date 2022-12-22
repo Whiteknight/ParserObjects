c2bc7e70ddd3de48fae35982027940fbda7c8c00
Whiteknight <wknight8111@gmail.com>
2022-12-21 19:42:28 -0500

    Peek() tests

---
+++ ParserObjects.Tests/Parsers/PeekTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using static ParserObjects.Parsers<char>;
+using static ParserObjects.Sequences;
+
+namespace ParserObjects.Tests.Parsers;
+
+public class PeekTests
+{
+    [Test]
+    public void Parse_Test()
+    {
+        var target = Peek();
+        var input = FromString("abc");
+        target.Parse(input).Value.Should().Be('a');
+        target.Parse(input).Value.Should().Be('a');
+        target.Parse(input).Value.Should().Be('a');
+        input.Consumed.Should().Be(0);
+    }
+
+    [Test]
+    public void Match_Test()
+    {
+        var target = Peek();
+        var input = FromString("abc");
+        target.Match(input).Should().BeTrue();
+        target.Match(input).Should().BeTrue();
+        target.Match(input).Should().BeTrue();
+        input.Consumed.Should().Be(0);
+    }
+
+    [Test]
+    public void GetChildren_Test()
+    {
+        var target = Peek();
+        target.GetChildren().Count().Should().Be(0);
+    }
+}
