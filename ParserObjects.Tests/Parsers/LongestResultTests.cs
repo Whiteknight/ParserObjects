ac4680d13567710465f4f8d0b26c76660768a696
Whiteknight <wknight8111@gmail.com>
2022-12-23 07:57:15 -0500

    Tests for multi parsers FirstResult() and LongestResult()

---
+++ ParserObjects.Tests/Parsers/LongestResultTests.cs
@@ -0,0 +1,22 @@
+﻿using static ParserObjects.Parsers;
+using static ParserObjects.Parsers<char>;
+
+namespace ParserObjects.Tests.Parsers;
+
+public class LongestResultTests
+{
+    [Test]
+    public void Parse_Test()
+    {
+        var parser = LongestResult(
+            Each(
+                CharacterString("a"),
+                CharacterString("ab"),
+                CharacterString("abc")
+            )
+        );
+        var result = parser.Parse("abc");
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("abc");
+    }
+}
