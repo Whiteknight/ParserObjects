5644d22075d23d9893e0cd7bf1b1d5b014abfa33
Whiteknight <wknight8111@gmail.com>
2023-01-05 18:34:41 -0500

    Prototype a Compose() parser
    Compose takes IParser<TA, TB> and IParser<TB, TC> returns IParser<TA, TC>
    
    This could be used to simplify a few of our example parsers and other scenarios

---
+++ ParserObjects.Tests/Parsers/ComposeTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using static ParserObjects.Sequences;
+using static ParserObjects.Parsers;
+
+namespace ParserObjects.Tests.Parsers;
+
+public class ComposeTests
+{
+    [Test]
+    public void Parse_Test()
+    {
+        var inner = Parsers<byte>.Any().Transform(b => (char)b);
+        var outer = CharacterString("abc");
+        var target = Compose(inner, outer);
+
+        var input = FromList(new byte[] { 97, 98, 99 });
+        var result = target.Parse(input);
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("abc");
+    }
+
+    [Test]
+    public void Parse_OuterFail()
+    {
+        var inner = Parsers<byte>.Any().Transform(b => (char)b);
+        var outer = CharacterString("XYZ");
+        var target = Compose(inner, outer);
+
+        var input = FromList(new byte[] { 97, 98, 99 });
+        var result = target.Parse(input);
+        result.Success.Should().BeFalse();
+    }
+
+    [Test]
+    public void Parse_InnerFail()
+    {
+        var inner = Parsers<byte>.Match(b => b >= 97 && b <= 99).Transform(b => (char)b);
+        var outer = CharacterString("abc");
+        var target = Compose(inner, outer);
+
+        var input = FromList(new byte[] { 10, 11, 12 });
+        var result = target.Parse(input);
+        result.Success.Should().BeFalse();
+    }
+
+    [Test]
+    public void Parse_Empty()
+    {
+        var inner = Parsers<byte>.Any().Transform(b => (char)b);
+        var outer = CharacterString("abc");
+        var target = Compose(inner, outer);
+
+        var input = FromList(Array.Empty<byte>());
+        var result = target.Parse(input);
+        result.Success.Should().BeFalse();
+    }
+
+    [Test]
+    public void Match_Test()
+    {
+        var inner = Parsers<byte>.Any().Transform(b => (char)b);
+        var outer = CharacterString("abc");
+        var target = Compose(inner, outer);
+
+        var input = FromList(new byte[] { 97, 98, 99 });
+        var result = target.Match(input);
+        result.Should().BeTrue();
+    }
+
+    [Test]
+    public void GetChildren_Test()
+    {
+        var inner = Parsers<byte>.Any().Transform(b => (char)b);
+        var outer = CharacterString("abc");
+        var target = Compose(inner, outer);
+
+        var result = target.GetChildren().ToList();
+        result.Count.Should().Be(2);
+        result.Should().Contain(inner);
+        result.Should().Contain(outer);
+    }
+}
---
+++ ParserObjects/Internal/Parsers/ComposeParser.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using static ParserObjects.Sequences;
+
+namespace ParserObjects.Internal.Parsers;
+
+public sealed record class ComposeParser<TInput, TMiddle, TOutput>(
+    IParser<TInput, TMiddle> Lexer,
+    IParser<TMiddle, TOutput> Parser,
+    Func<ResultFactory<TInput, TMiddle>, IResult<TMiddle>>? OnEnd,
+    string Name = ""
+) : IParser<TInput, TOutput>
+{
+    public int Id { get; } = UniqueIntegerGenerator.GetNext();
+
+    public IEnumerable<IParser> GetChildren() => new IParser[] { Lexer, Parser };
+
+    public bool Match(IParseState<TInput> state) => Parse(state).Success;
+
+    public IResult<TOutput> Parse(IParseState<TInput> state)
+    {
+        var startCp = state.Input.Checkpoint();
+        var innerState = GetInnerState(state);
+
+        // If the Inner parser fails for whatever reason, it returns an error result, the sequence
+        // will throw an InnerParserFailedException, and this parser returns a failure with
+        // necessary details.
+
+        // If we provide an OnEnd, that will be used to generate end sentinel values for the inner
+        // sequence, which should avoid throwing an error in that case. If we do not provide OnEnd,
+        // we will be at the mercy of the inner parser's behavior with regards to end of input.
+        // If the inner parser returns an error, it will generate an exception and bail out with
+        // an error here. If the inner parser returns a graceful success result with it's own
+        // end-of-input value, the outer parser may be able to transform that into something
+        // meaningful.
+
+        try
+        {
+            var outerResult = Parser.Parse(innerState);
+            if (!outerResult.Success)
+                startCp.Rewind();
+            return outerResult;
+        }
+        catch (InnerParserFailedException ipf)
+        {
+            startCp.Rewind();
+            return state.Fail(this, ipf.ErrorResult.ErrorMessage, new ResultData(ipf.ErrorResult));
+        }
+    }
+
+    private IParseState<TMiddle> GetInnerState(IParseState<TInput> state)
+    {
+        var cached = state.Cache.Get<IParseState<TMiddle>>(this, default);
+        if (cached.Success)
+            return cached.Value;
+
+        var innerSequence = FromParseResult(state.Input, Lexer, getEndSentinel: OnEnd)
+            .Select(static r =>
+            {
+                if (r.Success)
+                    return r.Value;
+                throw new InnerParserFailedException(r);
+            });
+        IParseState<TMiddle> innerState = new ParseState<TMiddle>(innerSequence, s => state.Log(this, s));
+        state.Cache.Add(this, default, innerState);
+        return innerState;
+    }
+
+    public INamed SetName(string name) => this with { Name = name };
+
+    IResult IParser<TInput>.Parse(IParseState<TInput> state) => Parse(state);
+
+    public void Visit<TVisitor, TState>(TVisitor visitor, TState state)
+        where TVisitor : IVisitor<TState>
+    {
+        throw new NotImplementedException();
+    }
+
+    private class InnerParserFailedException : ControlFlowException
+    {
+        public InnerParserFailedException(IResult<TMiddle> errorResult)
+            : base(errorResult.ErrorMessage)
+        {
+            ErrorResult = errorResult;
+        }
+
+        public IResult<TMiddle> ErrorResult { get; }
+    }
+}
