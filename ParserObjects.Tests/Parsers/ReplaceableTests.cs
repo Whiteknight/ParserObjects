cb46ce60adf614cd758d972b28060e1dc9454809
Whiteknight <wknight8111@gmail.com>
2023-01-16 16:47:54 -0500

    Expand Replaceable() coverage a bit

--- ParserObjects.Tests/Parsers/ReplaceableTests.cs
+++ ParserObjects.Tests/Parsers/ReplaceableTests.cs
@@ -2,124 +2,143 @@
 using static ParserObjects.Parsers<char>;
 using static ParserObjects.Sequences;
 
-namespace ParserObjects.Tests.Parsers
+namespace ParserObjects.Tests.Parsers;
+
+public static class ReplaceableTests
 {
-    public static class ReplaceableTests
+    public class SingleOutput
     {
-        public class SingleOutput
+        [Test]
+        public void Parse_Test()
         {
-            [Test]
-            public void Parse_Test()
-            {
-                var anyParser = Any();
-                var target = Replaceable(anyParser);
-                var input = FromString("abc");
-                var result = target.Parse(input);
-                result.Value.Should().Be('a');
-                result.Consumed.Should().Be(1);
-            }
-
-            [Test]
-            public void Parse_Fail()
-            {
-                var anyParser = Fail<char>();
-                var target = Replaceable(anyParser);
-                var input = FromString("abc");
-                var result = target.Parse(input);
-                result.Success.Should().BeFalse();
-                result.Consumed.Should().Be(0);
-            }
-
-            [Test]
-            public void Parse_Null()
-            {
-                var target = Replaceable((IParser<char, char>)null);
-                var input = FromString("abc");
-                var result = target.Parse(input);
-                result.Success.Should().BeFalse();
-                result.Consumed.Should().Be(0);
-            }
-
-            [Test]
-            public void SetParser_Test()
-            {
-                var anyParser = Any();
-                var failParser = Fail<char>();
-                var target = Replaceable(failParser);
-                (target as IReplaceableParserUntyped)?.SetParser(anyParser);
-                var input = FromString("abc");
-                var result = target.Parse(input);
-                result.Value.Should().Be('a');
-            }
-
-            [Test]
-            public void GetChildren_Test()
-            {
-                var anyParser = Any();
-                var target = Replaceable(anyParser);
-                var result = target.GetChildren().ToList();
-                result.Count.Should().Be(1);
-                result[0].Should().BeSameAs(anyParser);
-            }
-
-            [Test]
-            public void ToBnf_Test()
-            {
-                var parser = Replaceable(Any()).Named("parser");
-                var result = parser.ToBnf();
-                result.Should().Contain("parser := .");
-            }
+            var anyParser = Any();
+            var target = Replaceable(anyParser);
+            var input = FromString("abc");
+            var result = target.Parse(input);
+            result.Value.Should().Be('a');
+            result.Consumed.Should().Be(1);
         }
 
-        public class SingleNoOutput
+        [Test]
+        public void Parse_Fail()
         {
-            [Test]
-            public void Parse_Test()
-            {
-                var target = Replaceable(End());
-                var input = FromString("");
-                var result = target.Parse(input);
-                result.Success.Should().BeTrue();
-            }
-
-            [Test]
-            public void GetChildren_Test()
-            {
-                var end = End();
-                var target = Replaceable(end);
-                var result = target.GetChildren().ToList();
-                result.Count.Should().Be(1);
-                result[0].Should().BeSameAs(end);
-            }
-
-            [Test]
-            public void Parse_Null()
-            {
-                var target = Replaceable((IParser<char>)null);
-                var input = FromString("abc");
-                var result = target.Parse(input);
-                result.Success.Should().BeFalse();
-                result.Consumed.Should().Be(0);
-            }
-
-            [Test]
-            public void ToBnf_Test()
-            {
-                var parser = Replaceable(End()).Named("parser");
-                var result = parser.ToBnf();
-                result.Should().Contain("parser := END");
-            }
+            var anyParser = Fail<char>();
+            var target = Replaceable(anyParser);
+            var input = FromString("abc");
+            var result = target.Parse(input);
+            result.Success.Should().BeFalse();
+            result.Consumed.Should().Be(0);
         }
 
-        public class Multi
+        [Test]
+        public void Parse_Null()
         {
-            [Test]
-            public void ToBnf_Test()
-            {
-                var parser = Replaceable(ProduceMulti(() => new char[0])).Named("parser");
-                var result = parser.ToBnf();
-                result.Should().Contain("parser := PRODUCE");
-            }
+            var target = Replaceable((IParser<char, char>)null);
+            var input = FromString("abc");
+            var result = target.Parse(input);
+            result.Success.Should().BeFalse();
+            result.Consumed.Should().Be(0);
+        }
+
+        [Test]
+        public void SetParser_Test()
+        {
+            var anyParser = Any();
+            var failParser = Fail<char>();
+            var target = Replaceable(failParser);
+            (target as IReplaceableParserUntyped)?.SetParser(anyParser);
+            var input = FromString("abc");
+            var result = target.Parse(input);
+            result.Value.Should().Be('a');
+        }
+
+        [Test]
+        public void GetChildren_Test()
+        {
+            var anyParser = Any();
+            var target = Replaceable(anyParser);
+            var result = target.GetChildren().ToList();
+            result.Count.Should().Be(1);
+            result[0].Should().BeSameAs(anyParser);
+        }
+
+        [Test]
+        public void ToBnf_Test()
+        {
+            var parser = Replaceable(Any()).Named("parser");
+            var result = parser.ToBnf();
+            result.Should().Contain("parser := .");
+        }
+    }
+
+    public class SingleNoOutput
+    {
+        [Test]
+        public void Parse_Test()
+        {
+            var target = Replaceable(End());
+            var input = FromString("");
+            var result = target.Parse(input);
+            result.Success.Should().BeTrue();
+        }
+
+        [Test]
+        public void GetChildren_Test()
+        {
+            var end = End();
+            var target = Replaceable(end);
+            var result = target.GetChildren().ToList();
+            result.Count.Should().Be(1);
+            result[0].Should().BeSameAs(end);
+        }
+
+        [Test]
+        public void Parse_Null()
+        {
+            var target = Replaceable((IParser<char>)null);
+            var input = FromString("abc");
+            var result = target.Parse(input);
+            result.Success.Should().BeFalse();
+            result.Consumed.Should().Be(0);
+        }
+
+        [Test]
+        public void SetParser_Test()
+        {
+            var startParser = End();
+            var replaceParser = startParser.Named("replaced");
+            var target = Replaceable(startParser);
+            var result = (target as IReplaceableParserUntyped)?.SetParser(replaceParser);
+
+            var child = target.GetChildren().First();
+            child.Should().BeSameAs(replaceParser);
+            child.Should().NotBeSameAs(startParser);
+
+            result.Value.Success.Should().BeTrue();
+            var (success, old, newp, parent) = result.Value;
+            success.Should().BeTrue();
+            old.Should().BeSameAs(startParser);
+            newp.Should().BeSameAs(replaceParser);
+            parent.Should().BeSameAs(target);
+        }
+
+        [Test]
+        public void ToBnf_Test()
+        {
+            var parser = Replaceable(End()).Named("parser");
+            var result = parser.ToBnf();
+            result.Should().Contain("parser := END");
+        }
+    }
+
+    public class Multi
+    {
+        [Test]
+        public void ToBnf_Test()
+        {
+            var parser = Replaceable(ProduceMulti(() => new char[0])).Named("parser");
+            var result = parser.ToBnf();
+            result.Should().Contain("parser := PRODUCE");
         }
     }
 }
