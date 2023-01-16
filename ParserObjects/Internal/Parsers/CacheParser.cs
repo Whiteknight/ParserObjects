78da150fcce2ff4c28f3e36b1681594ecb61ea16
Whiteknight <wknight8111@gmail.com>
2023-01-16 11:47:49 -0500

    Refactor Cache parser to simplify and remove duplicated code

--- ParserObjects/Internal/Parsers/CacheParser.cs
+++ ParserObjects/Internal/Parsers/CacheParser.cs
@@ -6,129 +6,136 @@ namespace ParserObjects.Internal.Parsers;
 
 public static class Cache<TInput>
 {
-    public sealed record Parser(
-        IParser<TInput> Inner,
-        string Name = ""
-    ) : IParser<TInput>
+    private struct InternalParser<TParser, TResult, TCacheEntry>
+        where TParser : IParser
+        where TResult : IResultBase
     {
-        public int Id { get; } = UniqueIntegerGenerator.GetNext();
+        private readonly Func<TParser, IParseState<TInput>, TResult> _parse;
+        private readonly Func<TResult, SequenceCheckpoint, TCacheEntry> _getCacheEntry;
+        private readonly Func<TCacheEntry, (TResult, SequenceCheckpoint)> _decomposeCacheEntry;
 
-        public IEnumerable<IParser> GetChildren() => new[] { Inner };
-
-        public bool Match(IParseState<TInput> state)
+        public InternalParser(
+            TParser parser,
+            Func<TParser, IParseState<TInput>, TResult> parse,
+            Func<TResult, SequenceCheckpoint, TCacheEntry> getCacheEntry,
+            Func<TCacheEntry, (TResult, SequenceCheckpoint)> decomposeCacheEntry
+        )
         {
-            var location = state.Input.CurrentLocation;
-            var cached = state.Cache.Get<Tuple<IResult, SequenceCheckpoint>>(Inner, location);
-            if (cached.Success)
-            {
-                var (cachedResult, continuation) = cached.Value;
-                continuation.Rewind();
-                return cachedResult.Success;
-            }
-
-            var result = Inner.Parse(state);
-            state.Cache.Add(Inner, location, Tuple.Create(result, state.Input.Checkpoint()));
-            return result.Success;
+            Parser = parser;
+            _parse = parse;
+            _getCacheEntry = getCacheEntry;
+            _decomposeCacheEntry = decomposeCacheEntry;
         }
 
-        public IResult Parse(IParseState<TInput> state)
+        public TParser Parser { get; }
+
+        public TResult Parse(IParseState<TInput> state)
         {
-            var location = state.Input.CurrentLocation;
-            var cached = state.Cache.Get<Tuple<IResult, SequenceCheckpoint>>(Inner, location);
+            var startCp = state.Input.Checkpoint();
+            var cached = state.Cache.Get<TCacheEntry>(Parser, startCp.Location);
             if (!cached.Success)
             {
-                var result = Inner.Parse(state)!;
-                state.Cache.Add(Inner, location, Tuple.Create(result, state.Input.Checkpoint()));
+                var result = _parse(Parser, state);
+                var cacheKey = _getCacheEntry(result, startCp);
+                state.Cache.Add(Parser, startCp.Location, cacheKey);
                 return result;
             }
 
-            var (cachedResult, continuation) = cached.Value;
+            var (cachedResult, continuation) = _decomposeCacheEntry(cached.Value);
             if (!cachedResult.Success)
                 return cachedResult;
 
             continuation.Rewind();
             return cachedResult;
         }
+    }
 
-        public INamed SetName(string name) => this with { Name = name };
+    public sealed class Parser : IParser<TInput>
+    {
+        private readonly InternalParser<IParser<TInput>, IResult, Tuple<IResult, SequenceCheckpoint>> _internal;
+
+        public Parser(IParser<TInput> inner, string name = "")
+        {
+            _internal = new InternalParser<IParser<TInput>, IResult, Tuple<IResult, SequenceCheckpoint>>(
+                inner,
+                static (p, s) => p.Parse(s),
+                static (r, cp) => Tuple.Create(r, cp),
+                static ce => (ce.Item1, ce.Item2)
+            );
+            Name = name;
+        }
+
+        public int Id { get; } = UniqueIntegerGenerator.GetNext();
+
+        public string Name { get; }
+
+        public IEnumerable<IParser> GetChildren() => new[] { _internal.Parser };
+
+        public bool Match(IParseState<TInput> state) => _internal.Parse(state).Success;
+
+        public IResult Parse(IParseState<TInput> state) => _internal.Parse(state);
+
+        public INamed SetName(string name) => new Parser(_internal.Parser, name);
 
         public override string ToString() => DefaultStringifier.ToString(this);
     }
 
-    public sealed record Parser<TOutput>(
-        IParser<TInput, TOutput> Inner,
-        string Name = ""
-    ) : IParser<TInput, TOutput>
+    public sealed record Parser<TOutput> : IParser<TInput, TOutput>
     {
+        private readonly InternalParser<IParser<TInput, TOutput>, IResult<TOutput>, Tuple<IResult<TOutput>, SequenceCheckpoint>> _internal;
+
+        public Parser(IParser<TInput, TOutput> inner, string name = "")
+        {
+            _internal = new InternalParser<IParser<TInput, TOutput>, IResult<TOutput>, Tuple<IResult<TOutput>, SequenceCheckpoint>>(
+                inner,
+                static (p, s) => p.Parse(s),
+                static (r, cp) => Tuple.Create(r, cp),
+                static ce => (ce.Item1, ce.Item2)
+            );
+            Name = name;
+        }
+
         public int Id { get; } = UniqueIntegerGenerator.GetNext();
 
-        public IEnumerable<IParser> GetChildren() => new[] { Inner };
+        public string Name { get; }
 
-        public bool Match(IParseState<TInput> state)
-        {
-            var location = state.Input.CurrentLocation;
-            var cached = state.Cache.Get<Tuple<IResult, SequenceCheckpoint>>(Inner, location);
-            if (cached.Success)
-            {
-                var (cachedResult, continuation) = cached.Value;
-                continuation.Rewind();
-                return cachedResult.Success;
-            }
+        public IEnumerable<IParser> GetChildren() => new[] { _internal.Parser };
 
-            var result = Inner.Parse(state);
-            state.Cache.Add(Inner, location, Tuple.Create(result, state.Input.Checkpoint()));
-            return result.Success;
-        }
+        public bool Match(IParseState<TInput> state) => _internal.Parse(state).Success;
 
-        public IResult<TOutput> Parse(IParseState<TInput> state)
-        {
-            var location = state.Input.CurrentLocation;
-            var cached = state.Cache.Get<Tuple<IResult<TOutput>, SequenceCheckpoint>>(Inner, location);
-            if (!cached.Success)
-            {
-                var result = Inner.Parse(state);
-                state.Cache.Add(Inner, location, Tuple.Create(result, state.Input.Checkpoint()));
-                return result;
-            }
+        public IResult<TOutput> Parse(IParseState<TInput> state) => _internal.Parse(state);
 
-            var (cachedResult, continuation) = cached.Value;
-            if (!cachedResult.Success)
-                return cachedResult;
-
-            continuation.Rewind();
-
-            return cachedResult;
-        }
-
-        public INamed SetName(string name) => this with { Name = name };
+        public INamed SetName(string name) => new Parser<TOutput>(_internal.Parser, name);
 
         IResult IParser<TInput>.Parse(IParseState<TInput> state) => Parse(state);
 
         public override string ToString() => DefaultStringifier.ToString(this);
     }
 
-    public sealed record MultiParser<TOutput>(
-        IMultiParser<TInput, TOutput> Inner,
-        string Name = ""
-    ) : IMultiParser<TInput, TOutput>
+    public sealed class MultiParser<TOutput> : IMultiParser<TInput, TOutput>
     {
-        public int Id { get; } = UniqueIntegerGenerator.GetNext();
+        private readonly InternalParser<IMultiParser<TInput, TOutput>, IMultiResult<TOutput>, IMultiResult<TOutput>> _internal;
 
-        public IEnumerable<IParser> GetChildren() => new[] { Inner };
-
-        public IMultiResult<TOutput> Parse(IParseState<TInput> state)
+        public MultiParser(IMultiParser<TInput, TOutput> inner, string name = "")
         {
-            var location = state.Input.CurrentLocation;
-            var cached = state.Cache.Get<IMultiResult<TOutput>>(Inner, location);
-            if (cached.Success)
-                return cached.Value;
-
-            var result = Inner.Parse(state);
-            state.Cache.Add(Inner, location, result);
-            return result;
+            _internal = new InternalParser<IMultiParser<TInput, TOutput>, IMultiResult<TOutput>, IMultiResult<TOutput>>(
+                inner,
+                static (p, s) => p.Parse(s),
+                static (r, _) => r,
+                static ce => (ce, ce.StartCheckpoint)
+            );
+            Name = name;
         }
 
-        public INamed SetName(string name) => this with { Name = name };
+        public int Id { get; } = UniqueIntegerGenerator.GetNext();
+
+        public string Name { get; }
+
+        public IEnumerable<IParser> GetChildren() => new[] { _internal.Parser };
+
+        public IMultiResult<TOutput> Parse(IParseState<TInput> state) => _internal.Parse(state);
+
+        public INamed SetName(string name) => new MultiParser<TOutput>(_internal.Parser, name);
 
         IMultiResult IMultiParser<TInput>.Parse(IParseState<TInput> state) => Parse(state);
 
