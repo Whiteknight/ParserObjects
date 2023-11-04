2a85689414c0ca3e88a9222b0bb0613f26dcd104
Whiteknight <wknight8111@gmail.com>
2023-11-04 11:15:39 -0400

    Tests for UpperCase, LowerCase, Symbol and Stringify

---
+++ ParserObjects.Tests/Parsers/LowerCaseTests.cs
@@ -0,0 +1,20 @@
+﻿using static ParserObjects.Parsers;
+
+namespace ParserObjects.Tests.Parsers;
+
+public class LowerCaseTests
+{
+    [TestCase("A", false)]
+    [TestCase("a", true)]
+    [TestCase("1", false)]
+    [TestCase(" ", false)]
+    [TestCase("$", false)]
+    public void Test(string input, bool shouldMatch)
+    {
+        var target = LowerCase();
+        var result = target.Parse(input);
+        result.Success.Should().Be(shouldMatch);
+        if (shouldMatch)
+            result.Value.Should().Be(input[0]);
+    }
+}
---
+++ ParserObjects.Tests/Parsers/StringifyTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using static ParserObjects.Parsers;
+using static ParserObjects.Parsers<char>;
+
+namespace ParserObjects.Tests.Parsers;
+
+public static class StringifyTests
+{
+    public class Array
+    {
+        [Test]
+        public void Test()
+        {
+            var target = Stringify(Produce(() => new[] { 'a', 'b', 'c' }));
+            var result = target.Parse("");
+            result.Success.Should().BeTrue();
+            result.Value.Should().Be("abc");
+        }
+    }
+
+    public class List
+    {
+        [Test]
+        public void Test()
+        {
+            var target = Stringify(Produce(() => new List<char> { 'a', 'b', 'c' }));
+            var result = target.Parse("");
+            result.Success.Should().BeTrue();
+            result.Value.Should().Be("abc");
+        }
+    }
+}
