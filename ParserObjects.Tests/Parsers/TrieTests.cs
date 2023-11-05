089ede5b48554c148bb0d4dfd4ffee522484317b
Whiteknight <wknight8111@gmail.com>
2023-11-05 06:24:47 -0500

    Fix up Trie tests
    Rearrange and rename a few things to be more in line with how we test other parsers
    Remove a few duplicate tests
    Add a test for TrieMulti with no possibilities

--- ParserObjects.Tests/Parsers/TrieTests.cs
+++ ParserObjects.Tests/Parsers/TrieTests.cs
@@ -6,8 +6,96 @@ namespace ParserObjects.Tests.Parsers;
 
 public static class TrieTests
 {
-    public class SingleOperators
+    public class MultiAction
     {
+        [Test]
+        public void Parse()
+        {
+            var target = TrieMulti<string>(trie => trie
+                .Add("=", "=")
+                .Add("==", "==")
+                .Add("===", "===")
+                .Add(">=", ">=")
+                .Add("<=", "<=")
+                .Add("<", "<")
+                .Add(">", ">")
+            );
+
+            var input = FromString("===>=<=><<==");
+
+            var result = target.Parse(input);
+            result.Success.Should().BeTrue();
+            result.Results.Count.Should().Be(3);
+            result.Results[0].Value.Should().Be("=");
+            result.Results[0].Consumed.Should().Be(1);
+            result.Results[1].Value.Should().Be("==");
+            result.Results[1].Consumed.Should().Be(2);
+            result.Results[2].Value.Should().Be("===");
+            result.Results[2].Consumed.Should().Be(3);
+        }
+
+        [Test]
+        public void Parse_Continue()
+        {
+            static void Populate(InsertableTrie<char, string> trie)
+            {
+                trie.Add("=", "=");
+                trie.Add("==", "==");
+                trie.Add("===", "===");
+                trie.Add(">=", ">=");
+                trie.Add("<=", "<=");
+                trie.Add("<", "<");
+                trie.Add(">", ">");
+            }
+
+            var multiTrieParser = TrieMulti<string>(Populate);
+            var singleTrieParser = Trie<string>(Populate);
+
+            var target = multiTrieParser.ContinueWith(left => Rule(
+                left,
+                singleTrieParser,
+                (l, r) => $"{l} {r}"
+            ));
+            var input = FromString("===>=<=><<==");
+
+            var result = target.Parse(input);
+            result.Success.Should().BeTrue();
+            result.Results.Count.Should().Be(3);
+            result.Results[0].Value.Should().Be("= ==");
+            result.Results[1].Value.Should().Be("== =");
+            result.Results[2].Value.Should().Be("=== >=");
+        }
+    }
+
+    public class MultiInstance
+    {
+        [Test]
+        public void Parse_EmptyTrie()
+        {
+            var trie = InsertableTrie<char, int>.Create();
+            var target = TrieMulti(trie);
+
+            var result = target.Parse("abc");
+            result.Success.Should().BeFalse();
+        }
+    }
+
+    public class SingleAction
+    {
+        [Test]
+        public void Parse_IsAtEnd()
+        {
+            var target = Trie<string>(trie => trie
+                .Add("\0\0\0", "ok")
+            );
+
+            var input = FromString("\0");
+
+            var result = target.Parse(input);
+            result.Success.Should().BeFalse();
+            result.Consumed.Should().Be(0);
+        }
+
         [Test]
         public void Parse_Operators()
         {
@@ -52,55 +140,6 @@ public static class TrieTests
         }
 
         [Test]
-        public void Single_Action_Operators()
-        {
-            var target = Trie<string>(trie => trie
-                .Add("=", "=")
-                .Add("==", "==")
-                .Add(">=", ">=")
-                .Add("<=", "<=")
-                .Add("<", "<")
-                .Add(">", ">")
-            );
-
-            var input = FromString("===>=<=><<==");
-
-            target.Parse(input).Value.Should().Be("==");
-            target.Parse(input).Value.Should().Be("=");
-            target.Parse(input).Value.Should().Be(">=");
-            target.Parse(input).Value.Should().Be("<=");
-            target.Parse(input).Value.Should().Be(">");
-            target.Parse(input).Value.Should().Be("<");
-            target.Parse(input).Value.Should().Be("<=");
-            target.Parse(input).Value.Should().Be("=");
-        }
-
-        [Test]
-        public void Parse_Operators_Method()
-        {
-            var target = Trie<string>(trie =>
-            {
-                trie.Add("=", "=");
-                trie.Add("==", "==");
-                trie.Add(">=", ">=");
-                trie.Add("<=", "<=");
-                trie.Add("<", "<");
-                trie.Add(">", ">");
-            });
-
-            var input = FromString("===>=<=><<==");
-
-            target.Parse(input).Value.Should().Be("==");
-            target.Parse(input).Value.Should().Be("=");
-            target.Parse(input).Value.Should().Be(">=");
-            target.Parse(input).Value.Should().Be("<=");
-            target.Parse(input).Value.Should().Be(">");
-            target.Parse(input).Value.Should().Be("<");
-            target.Parse(input).Value.Should().Be("<=");
-            target.Parse(input).Value.Should().Be("=");
-        }
-
-        [Test]
         public void Parse_Consumed()
         {
             var target = Trie<string>(trie => trie
@@ -125,67 +164,6 @@ public static class TrieTests
             result = target.Parse(input);
             result.Success.Should().BeFalse();
             result.Consumed.Should().Be(0);
-        }
-    }
-
-    public class MultiOperators
-    {
-        [Test]
-        public void Multi_Action_Operators()
-        {
-            var target = TrieMulti<string>(trie => trie
-                .Add("=", "=")
-                .Add("==", "==")
-                .Add("===", "===")
-                .Add(">=", ">=")
-                .Add("<=", "<=")
-                .Add("<", "<")
-                .Add(">", ">")
-            );
-
-            var input = FromString("===>=<=><<==");
-
-            var result = target.Parse(input);
-            result.Success.Should().BeTrue();
-            result.Results.Count.Should().Be(3);
-            result.Results[0].Value.Should().Be("=");
-            result.Results[0].Consumed.Should().Be(1);
-            result.Results[1].Value.Should().Be("==");
-            result.Results[1].Consumed.Should().Be(2);
-            result.Results[2].Value.Should().Be("===");
-            result.Results[2].Consumed.Should().Be(3);
-        }
-
-        [Test]
-        public void Multi_Action_Continue()
-        {
-            void Populate(InsertableTrie<char, string> trie)
-            {
-                trie.Add("=", "=");
-                trie.Add("==", "==");
-                trie.Add("===", "===");
-                trie.Add(">=", ">=");
-                trie.Add("<=", "<=");
-                trie.Add("<", "<");
-                trie.Add(">", ">");
-            }
-
-            var multiTrieParser = TrieMulti<string>(Populate);
-            var singleTrieParser = Trie<string>(Populate);
-
-            var target = multiTrieParser.ContinueWith(left => Rule(
-                left,
-                singleTrieParser,
-                (l, r) => $"{l} {r}"
-            ));
-            var input = FromString("===>=<=><<==");
-
-            var result = target.Parse(input);
-            result.Success.Should().BeTrue();
-            result.Results.Count.Should().Be(3);
-            result.Results[0].Value.Should().Be("= ==");
-            result.Results[1].Value.Should().Be("== =");
-            result.Results[2].Value.Should().Be("=== >=");
         }
 
         [TestCase("=")]
@@ -228,93 +206,6 @@ public static class TrieTests
             var input = FromString("X");
             target.Match(input).Should().BeFalse();
             input.Consumed.Should().Be(0);
-        }
-    }
-
-    public class Single
-    {
-        [Test]
-        public void Parse_IsAtEnd()
-        {
-            var target = Trie<string>(trie => trie
-                .Add("\0\0\0", "ok")
-            );
-
-            var input = FromString("\0");
-
-            var result = target.Parse(input);
-            result.Success.Should().BeFalse();
-            result.Consumed.Should().Be(0);
-        }
-
-        [Test]
-        public void Parse_StringInt()
-        {
-            var trie = InsertableTrie<char, int>.Create();
-            trie.Add("abc", 1);
-            trie.Add("abd", 2);
-            trie.Add("aef", 3);
-            trie.Add("aeg", 4);
-            trie.Add("hij", 5);
-            var target = Trie(trie);
-
-            var input = FromString("abcabdaefaeghij");
-            target.Parse(input).Value.Should().Be(1);
-            target.Parse(input).Value.Should().Be(2);
-            target.Parse(input).Value.Should().Be(3);
-            target.Parse(input).Value.Should().Be(4);
-            target.Parse(input).Value.Should().Be(5);
-        }
-
-        [Test]
-        public void Parse_StringInt_AddGetBacktrack()
-        {
-            var trie = InsertableTrie<char, int>.Create();
-            trie.Add("abc", 1);
-            trie.Add("abcde", 2);
-            var target = Trie(trie);
-
-            // looks for "abcd", has a node but no value. Then backtracks to "abc" and finds the value
-            var input = FromString("abcd");
-
-            target.Parse(input).Value.Should().Be(1);
-        }
-
-        [TestCase("a", 1)]
-        [TestCase("ab", 2)]
-        [TestCase("abc", 3)]
-        [TestCase("abcd", 4)]
-        public void Parse_StringInt_AddGetPrefixes(string input, int result)
-        {
-            var trie = InsertableTrie<char, int>.Create();
-            trie.Add("a", 1);
-            trie.Add("ab", 2);
-            trie.Add("abc", 3);
-            trie.Add("abcd", 4);
-
-            var target = Trie(trie);
-            target.Parse(input).Value.Should().Be(result);
-        }
-
-        [Test]
-        public void Parse_StringInt_Duplicate_WrongValue()
-        {
-            var trie = InsertableTrie<char, int>.Create();
-            trie.Add("abc", 1);
-
-            Action act = () => trie.Add("abc", 2);
-            act.Should().Throw<TrieInsertException>();
-        }
-
-        [Test]
-        public void Parse_StringInt_Duplicate_SameValue()
-        {
-            var trie = InsertableTrie<char, int>.Create();
-            trie.Add("abc", 1);
-            trie.Add("abc", 1);
-
-            var target = Trie(trie);
-            target.Parse("abc").Value.Should().Be(1);
         }
 
         [Test]
@@ -367,6 +258,89 @@ public static class TrieTests
             var parser = Trie<string>(trie => { }).Named("parser");
             var result = parser.ToBnf();
             result.Should().Contain("parser := FAIL");
+        }
+    }
+
+    public class SingleInstance
+    {
+        [Test]
+        public void Parse_StringInt()
+        {
+            var trie = InsertableTrie<char, int>.Create();
+            trie.Add("abc", 1);
+            trie.Add("abd", 2);
+            trie.Add("aef", 3);
+            trie.Add("aeg", 4);
+            trie.Add("hij", 5);
+            var target = Trie(trie);
+
+            var input = FromString("abcabdaefaeghij");
+            target.Parse(input).Value.Should().Be(1);
+            target.Parse(input).Value.Should().Be(2);
+            target.Parse(input).Value.Should().Be(3);
+            target.Parse(input).Value.Should().Be(4);
+            target.Parse(input).Value.Should().Be(5);
+        }
+
+        [Test]
+        public void Parse_EmptyTrie()
+        {
+            var trie = InsertableTrie<char, int>.Create();
+            var target = Trie(trie);
+
+            var result = target.Parse("abc");
+            result.Success.Should().BeFalse();
+        }
+
+        [Test]
+        public void Parse_StringInt_AddGetBacktrack()
+        {
+            var trie = InsertableTrie<char, int>.Create();
+            trie.Add("abc", 1);
+            trie.Add("abcde", 2);
+            var target = Trie(trie);
+
+            // looks for "abcd", has a node but no value. Then backtracks to "abc" and finds the value
+            var input = FromString("abcd");
+
+            target.Parse(input).Value.Should().Be(1);
+        }
+
+        [TestCase("a", 1)]
+        [TestCase("ab", 2)]
+        [TestCase("abc", 3)]
+        [TestCase("abcd", 4)]
+        public void Parse_StringInt_AddGetPrefixes(string input, int result)
+        {
+            var trie = InsertableTrie<char, int>.Create();
+            trie.Add("a", 1);
+            trie.Add("ab", 2);
+            trie.Add("abc", 3);
+            trie.Add("abcd", 4);
+
+            var target = Trie(trie);
+            target.Parse(input).Value.Should().Be(result);
+        }
+
+        [Test]
+        public void Parse_StringInt_Duplicate_WrongValue()
+        {
+            var trie = InsertableTrie<char, int>.Create();
+            trie.Add("abc", 1);
+
+            Action act = () => trie.Add("abc", 2);
+            act.Should().Throw<TrieInsertException>();
+        }
+
+        [Test]
+        public void Parse_StringInt_Duplicate_SameValue()
+        {
+            var trie = InsertableTrie<char, int>.Create();
+            trie.Add("abc", 1);
+            trie.Add("abc", 1);
+
+            var target = Trie(trie);
+            target.Parse("abc").Value.Should().Be(1);
         }
     }
 }
