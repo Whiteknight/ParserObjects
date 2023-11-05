91002bc1fbf3ffa489dc526938095029174a836b
Whiteknight <wknight8111@gmail.com>
2023-11-05 06:10:53 -0500

    Tests for CaptureString
    Break CaptureString tests out from Capture tests into their own file
    Add some tests for some cases that are missing

---
+++ ParserObjects.Tests/Parsers/CaptureStringTests.cs
@@ -0,0 +1,63 @@
+﻿using static ParserObjects.Parsers;
+using static ParserObjects.Parsers<char>;
+using static ParserObjects.Sequences;
+
+namespace ParserObjects.Tests.Parsers;
+
+public class CaptureStringTests
+{
+    [Test]
+    public void Parse_Test()
+    {
+        var target = CaptureString(
+            MatchChars("abc"),
+            Any(),
+            Any(),
+            Any(),
+            MatchChars("ghi")
+        );
+        var result = target.Parse("abcdefghi");
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("abcdefghi");
+    }
+
+    [Test]
+    public void Parse_NonCharSequence()
+    {
+        var target = CaptureString(
+            MatchChar('a'),
+            Any(),
+            MatchChar('c')
+        );
+        var input = FromEnumerable(new[] { (byte)'a', (byte)'b', (byte)'c' }).Select(b => (char)b);
+        var result = target.Parse(input);
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("abc");
+    }
+
+    [Test]
+    public void Parse_Empty()
+    {
+        var target = CaptureString();
+        var result = target.Parse("abcdefghi");
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("");
+    }
+
+    [Test]
+    public void Parse_Null()
+    {
+        var target = CaptureString(null);
+        var result = target.Parse("abcdefghi");
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("");
+    }
+
+    [Test]
+    public void ToBnf_Test()
+    {
+        var target = Capture(Any()).Named("SUT");
+        var result = target.ToBnf();
+        result.Should().Contain("SUT := (.)");
+    }
+}
--- ParserObjects.Tests/Parsers/CaptureTests.cs
+++ ParserObjects.Tests/Parsers/CaptureTests.cs
@@ -3,75 +3,37 @@ using static ParserObjects.Parsers;
 
 namespace ParserObjects.Tests.Parsers;
 
-public static class CaptureTests
+public class CaptureTests
 {
-    public class Inputs
+    [Test]
+    public void Parse_Test()
     {
-        [Test]
-        public void Parse_Test()
-        {
-            var target = Capture(
-                MatchChars("abc"),
-                Any(),
-                Any(),
-                Any(),
-                MatchChars("ghi")
-            ).Transform(c => new string(c));
-            var result = target.Parse("abcdefghi");
-            result.Success.Should().BeTrue();
-            result.Value.Should().Be("abcdefghi");
-        }
-
-        [Test]
-        public void Parse_Empty()
-        {
-            var target = Capture().Transform(c => new string(c));
-            var result = target.Parse("abcdefghi");
-            result.Success.Should().BeTrue();
-            result.Value.Should().Be("");
-        }
-
-        [Test]
-        public void ToBnf_Test()
-        {
-            var target = Capture(Any()).Named("SUT");
-            var result = target.ToBnf();
-            result.Should().Contain("SUT := (.)");
-        }
+        var target = Capture(
+            MatchChars("abc"),
+            Any(),
+            Any(),
+            Any(),
+            MatchChars("ghi")
+        ).Transform(c => new string(c));
+        var result = target.Parse("abcdefghi");
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("abcdefghi");
     }
 
-    public class CharString
+    [Test]
+    public void Parse_Empty()
     {
-        [Test]
-        public void Parse_Test()
-        {
-            var target = CaptureString(
-                MatchChars("abc"),
-                Any(),
-                Any(),
-                Any(),
-                MatchChars("ghi")
-            );
-            var result = target.Parse("abcdefghi");
-            result.Success.Should().BeTrue();
-            result.Value.Should().Be("abcdefghi");
-        }
+        var target = Capture().Transform(c => new string(c));
+        var result = target.Parse("abcdefghi");
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be("");
+    }
 
-        [Test]
-        public void Parse_Empty()
-        {
-            var target = CaptureString();
-            var result = target.Parse("abcdefghi");
-            result.Success.Should().BeTrue();
-            result.Value.Should().Be("");
-        }
-
-        [Test]
-        public void ToBnf_Test()
-        {
-            var target = Capture(Any()).Named("SUT");
-            var result = target.ToBnf();
-            result.Should().Contain("SUT := (.)");
-        }
+    [Test]
+    public void ToBnf_Test()
+    {
+        var target = Capture(Any()).Named("SUT");
+        var result = target.ToBnf();
+        result.Should().Contain("SUT := (.)");
     }
 }
