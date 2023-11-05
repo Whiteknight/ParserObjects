094fa98b83d98a17a953d307cee88efd62beca37
Whiteknight <wknight8111@gmail.com>
2023-11-05 07:35:31 -0500

    Tests for .Or extension

--- ParserObjects.Tests/Parsers/OrTests.cs
+++ ParserObjects.Tests/Parsers/OrTests.cs
@@ -3,69 +3,89 @@ using static ParserObjects.Sequences;
 
 namespace ParserObjects.Tests.Parsers;
 
-public class OrTests
+public static class OrTests
 {
-    private readonly IParser<char, char> _anyParser = Any();
-    private readonly IParser<char, char> _failParser = Fail<char>();
+    private static readonly IParser<char, char> _anyParser = Any();
+    private static readonly IParser<char, char> _failParser = Fail<char>();
 
-    [TestCase(true, true)]
-    [TestCase(true, false)]
-    [TestCase(false, true)]
-    [TestCase(false, false)]
-    public void Parse_Test(bool firstSuccess, bool secondSuccess)
+    public class Function
     {
-        var p1 = firstSuccess ? _anyParser : _failParser;
-        var p2 = secondSuccess ? _anyParser : _failParser;
-        var target = Or(p1, p2);
-        var result = target.Parse("abc");
-        result.Success.Should().Be(firstSuccess | secondSuccess);
-        result.Consumed.Should().Be(firstSuccess | secondSuccess ? 1 : 0);
+        [TestCase(true, true)]
+        [TestCase(true, false)]
+        [TestCase(false, true)]
+        [TestCase(false, false)]
+        public void Parse_Test(bool firstSuccess, bool secondSuccess)
+        {
+            var p1 = firstSuccess ? _anyParser : _failParser;
+            var p2 = secondSuccess ? _anyParser : _failParser;
+            var target = Or(p1, p2);
+            var result = target.Parse("abc");
+            result.Success.Should().Be(firstSuccess | secondSuccess);
+            result.Consumed.Should().Be(firstSuccess | secondSuccess ? 1 : 0);
+        }
+
+        [Test]
+        public void Parse_Null()
+        {
+            var target = Or((IParser<char>[])null);
+            var result = target.Parse("abc");
+            result.Success.Should().BeFalse();
+        }
+
+        [Test]
+        public void Parse_Empty()
+        {
+            var target = Or();
+            var result = target.Parse("abc");
+            result.Success.Should().BeFalse();
+        }
+
+        [Test]
+        public void Parse_Single()
+        {
+            var target = Or(_anyParser);
+            var result = target.Parse("abc");
+            result.Success.Should().BeTrue();
+        }
+
+        [TestCase(true, true)]
+        [TestCase(true, false)]
+        [TestCase(false, true)]
+        [TestCase(false, false)]
+        public void Match_Test(bool firstSuccess, bool secondSuccess)
+        {
+            var p1 = firstSuccess ? _anyParser : _failParser;
+            var p2 = secondSuccess ? _anyParser : _failParser;
+            var target = Or(p1, p2);
+            var sequence = FromString("abc");
+            var result = target.Match(sequence);
+            result.Should().Be(firstSuccess | secondSuccess);
+            sequence.Consumed.Should().Be(firstSuccess | secondSuccess ? 1 : 0);
+        }
+
+        [Test]
+        public void ToBnf_Test()
+        {
+            var parser = Or(Any(), Any()).Named("parser");
+            var result = parser.ToBnf();
+            result.Should().Contain("parser := (. | .)");
+        }
     }
 
-    [Test]
-    public void Parse_Null()
+    public class Extension
     {
-        var target = Or((IParser<char>[])null);
-        var result = target.Parse("abc");
-        result.Success.Should().BeFalse();
-    }
-
-    [Test]
-    public void Parse_Empty()
-    {
-        var target = Or();
-        var result = target.Parse("abc");
-        result.Success.Should().BeFalse();
-    }
-
-    [Test]
-    public void Parse_Single()
-    {
-        var target = Or(_anyParser);
-        var result = target.Parse("abc");
-        result.Success.Should().BeTrue();
-    }
-
-    [TestCase(true, true)]
-    [TestCase(true, false)]
-    [TestCase(false, true)]
-    [TestCase(false, false)]
-    public void Match_Test(bool firstSuccess, bool secondSuccess)
-    {
-        var p1 = firstSuccess ? _anyParser : _failParser;
-        var p2 = secondSuccess ? _anyParser : _failParser;
-        var target = Or(p1, p2);
-        var sequence = FromString("abc");
-        var result = target.Match(sequence);
-        result.Should().Be(firstSuccess | secondSuccess);
-        sequence.Consumed.Should().Be(firstSuccess | secondSuccess ? 1 : 0);
-    }
-
-    [Test]
-    public void ToBnf_Test()
-    {
-        var parser = Or(Any(), Any()).Named("parser");
-        var result = parser.ToBnf();
-        result.Should().Contain("parser := (. | .)");
+        [TestCase(true, true)]
+        [TestCase(true, false)]
+        [TestCase(false, true)]
+        [TestCase(false, false)]
+        public void Parse_Test(bool firstSuccess, bool secondSuccess)
+        {
+            var p1 = firstSuccess ? _anyParser : _failParser;
+            var p2 = secondSuccess ? _anyParser : _failParser;
+            var target = p1.Or(p2);
+            var result = target.Parse("abc");
+            result.Success.Should().Be(firstSuccess | secondSuccess);
+            result.Consumed.Should().Be(firstSuccess | secondSuccess ? 1 : 0);
+        }
     }
 }
