ceb598e285ebd50d6be6f6bf4622231ecd369bd8
Whiteknight <wknight8111@gmail.com>
2024-10-13 10:38:57 -0400

    Tuple.Rule extensions are converted to use generated Parser<T>.Rule() methods

--- ParserObjects/Tuple.Rule.cs
+++ ParserObjects/Tuple.Rule.cs
@@ -18,15 +18,7 @@ public static partial class TupleExtensions
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>) parsers,
         Func<T1, T2, TOutput> produce
-    )
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1]),
-            false
-        );
-    }
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, produce);
 
     /// <summary>
     /// Execute the given parsers in order and use the output results in a production rule to
@@ -43,15 +35,7 @@ public static partial class TupleExtensions
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, T3, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>, IParser<TInput, T3>) parsers,
         Func<T1, T2, T3, TOutput> produce
-    )
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2, parsers.Item3 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1], (T3)list[2]),
-            false
-        );
-    }
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, parsers.Item3, produce);
 
     /// <summary>
     /// Execute the given parsers in order and use the output results in a production rule to
@@ -69,15 +53,7 @@ public static partial class TupleExtensions
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, T3, T4, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>, IParser<TInput, T3>, IParser<TInput, T4>) parsers,
         Func<T1, T2, T3, T4, TOutput> produce
-    )
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1], (T3)list[2], (T4)list[3]),
-            false
-        );
-    }
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, produce);
 
     /// <summary>
     /// Execute the given parsers in order and use the output results in a production rule to
@@ -95,15 +71,8 @@ public static partial class TupleExtensions
     /// <returns></returns>
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, T3, T4, T5, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>, IParser<TInput, T3>, IParser<TInput, T4>, IParser<TInput, T5>) parsers,
-        Func<T1, T2, T3, T4, T5, TOutput> produce)
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1], (T3)list[2], (T4)list[3], (T5)list[4]),
-            false
-        );
-    }
+        Func<T1, T2, T3, T4, T5, TOutput> produce
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, produce);
 
     /// <summary>
     /// Execute the given parsers in order and use the output results in a production rule to
@@ -123,15 +92,7 @@ public static partial class TupleExtensions
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, T3, T4, T5, T6, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>, IParser<TInput, T3>, IParser<TInput, T4>, IParser<TInput, T5>, IParser<TInput, T6>) parsers,
         Func<T1, T2, T3, T4, T5, T6, TOutput> produce
-    )
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1], (T3)list[2], (T4)list[3], (T5)list[4], (T6)list[5]),
-            false
-        );
-    }
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6, produce);
 
     /// <summary>
     /// Execute the given parsers in order and use the output results in a production rule to
@@ -152,15 +113,7 @@ public static partial class TupleExtensions
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, T3, T4, T5, T6, T7, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>, IParser<TInput, T3>, IParser<TInput, T4>, IParser<TInput, T5>, IParser<TInput, T6>, IParser<TInput, T7>) parsers,
         Func<T1, T2, T3, T4, T5, T6, T7, TOutput> produce
-    )
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6, parsers.Item7 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1], (T3)list[2], (T4)list[3], (T5)list[4], (T6)list[5], (T7)list[6]),
-            false
-        );
-    }
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6, parsers.Item7, produce);
 
     /// <summary>
     /// Execute the given parsers in order and use the output results in a production rule to
@@ -182,15 +135,7 @@ public static partial class TupleExtensions
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, T3, T4, T5, T6, T7, T8, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>, IParser<TInput, T3>, IParser<TInput, T4>, IParser<TInput, T5>, IParser<TInput, T6>, IParser<TInput, T7>, IParser<TInput, T8>) parsers,
         Func<T1, T2, T3, T4, T5, T6, T7, T8, TOutput> produce
-    )
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6, parsers.Item7, parsers.Item8 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1], (T3)list[2], (T4)list[3], (T5)list[4], (T6)list[5], (T7)list[6], (T8)list[7]),
-            false
-        );
-    }
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6, parsers.Item7, parsers.Item8, produce);
 
     /// <summary>
     /// Execute the given parsers in order and use the output results in a production rule to
@@ -213,13 +158,5 @@ public static partial class TupleExtensions
     public static IParser<TInput, TOutput> Rule<TInput, T1, T2, T3, T4, T5, T6, T7, T8, T9, TOutput>(
         this (IParser<TInput, T1>, IParser<TInput, T2>, IParser<TInput, T3>, IParser<TInput, T4>, IParser<TInput, T5>, IParser<TInput, T6>, IParser<TInput, T7>, IParser<TInput, T8>, IParser<TInput, T9>) parsers,
         Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TOutput> produce
-    )
-    {
-        return Internal.Parsers.Rule.Create(
-            new IParser<TInput>[] { parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6, parsers.Item7, parsers.Item8, parsers.Item9 },
-            produce,
-            static (p, list) => p((T1)list[0], (T2)list[1], (T3)list[2], (T4)list[3], (T5)list[4], (T6)list[5], (T7)list[6], (T8)list[7], (T9)list[8]),
-            false
-        );
-    }
+    ) => Parsers<TInput>.Rule(parsers.Item1, parsers.Item2, parsers.Item3, parsers.Item4, parsers.Item5, parsers.Item6, parsers.Item7, parsers.Item8, parsers.Item9, produce);
 }
