7edd0ccd7e04e4626f5c5a799a0e48d82a1c0b7f
Whiteknight <wknight8111@gmail.com>
2024-10-13 15:59:15 -0400

    wip Start implementing some parser base classes.
    I know, I know, I hate inheritance. I'm doing the experiment anyway to see if the reduction in lines of code is significant enough for me to suck it up and deal. So far, some of the simple parser types are showing some promising shortening, but I will have to see if I want to continue with the remainder.

--- ParserObjects/Internal/Parsers/EmptyParser.cs
+++ ParserObjects/Internal/Parsers/EmptyParser.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using ParserObjects.Internal.Visitors;
+﻿using ParserObjects.Internal.Visitors;
 
 namespace ParserObjects.Internal.Parsers;
 
@@ -8,33 +6,19 @@ namespace ParserObjects.Internal.Parsers;
 /// The empty parser, consumes no input and always returns success.
 /// </summary>
 /// <typeparam name="TInput"></typeparam>
-public sealed class EmptyParser<TInput> : IParser<TInput>
+public sealed record EmptyParser<TInput>(
+    string Name = ""
+) : SimpleRecordParser<TInput>(Name), IParser<TInput>
 {
-    public EmptyParser(string name = "")
-    {
-        Name = name;
-    }
-
-    public int Id { get; } = UniqueIntegerGenerator.GetNext();
-
-    public string Name { get; }
-
-    public Result<object> Parse(IParseState<TInput> state)
+    public override Result<object> Parse(IParseState<TInput> state)
     {
         Assert.ArgumentNotNull(state);
         return Result<object>.Ok(this, Defaults.ObjectInstance, 0);
     }
 
-    public bool Match(IParseState<TInput> state) => true;
+    public override bool Match(IParseState<TInput> state) => true;
 
-    public IEnumerable<IParser> GetChildren() => Enumerable.Empty<IParser>();
-
-    public override string ToString() => DefaultStringifier.ToString("Empty", Name, Id);
-
-    public INamed SetName(string name) => new EmptyParser<TInput>(name);
-
-    public void Visit<TVisitor, TState>(TVisitor visitor, TState state)
-        where TVisitor : IVisitor<TState>
+    public override void Visit<TVisitor, TState>(TVisitor visitor, TState state)
     {
         visitor.Get<IMatchPartialVisitor<TState>>()?.Accept(this, state);
     }
