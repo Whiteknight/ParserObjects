18134562c71c0d207d0d03910a8d5f8ce4d0ba99
Whiteknight <wknight8111@gmail.com>
2024-10-24 16:47:11 -0400

    Add tests for IsEnd()

---
+++ ParserObjects.Tests/Parsers/IsEndTests.cs
@@ -0,0 +1,27 @@
+﻿using static ParserObjects.Sequences;
+using static ParserObjects.Parsers<char>;
+
+namespace ParserObjects.Tests.Parsers;
+
+internal class IsEndTests
+{
+    [Test]
+    public void Test()
+    {
+        var input = FromString("ab");
+        var target = IsEnd();
+        var result = target.Parse(input);
+        result.Success.Should().BeTrue();
+        result.Value.Should().BeFalse();
+
+        input.GetNext().Should().Be('a');
+        result = target.Parse(input);
+        result.Success.Should().BeTrue();
+        result.Value.Should().BeFalse();
+
+        input.GetNext().Should().Be('b');
+        result = target.Parse(input);
+        result.Success.Should().BeTrue();
+        result.Value.Should().BeTrue();
+    }
+}
