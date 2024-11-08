38b3690168f2d26aa001c942ad63c024b84cdb44
Whiteknight <wknight8111@gmail.com>
2024-11-08 05:51:56 -0500

    Add parenthesis balancing example to test suite

---
+++ ParserObjects.Tests/Examples/Parenthesis/ParenthesisGrammar.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using static ParserObjects.Parsers;
+using static ParserObjects.Parsers<char>;
+
+namespace ParserObjects.Tests.Examples.Parenthesis;
+
+public static class ParenthesisGrammar
+{
+    public static IParser<char, bool> CreateParser()
+    {
+        IParser<char, bool> validatorInternal = null;
+        var validator = Deferred(() => validatorInternal);
+
+        var parenthesis = Rule(
+            MatchChar('('),
+            validator,
+            MatchChar(')'),
+            (_, inner, _) => inner && true
+        );
+
+        var braces = Rule(
+            MatchChar('{'),
+            validator,
+            MatchChar('}'),
+            (_, inner, _) => inner && true
+        );
+
+        var brackets = Rule(
+            MatchChar('['),
+            validator,
+            MatchChar(']'),
+            (_, inner, _) => inner && true
+        );
+
+        validatorInternal = First(
+            parenthesis,
+            braces,
+            brackets,
+            Produce(() => true)
+        ).List().Transform(x => x.All(y => y));
+
+        return Rule(
+            validatorInternal,
+            End(),
+            (r, _) => r
+        );
+    }
+}
---
+++ ParserObjects.Tests/Examples/Parenthesis/ParenthesisValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using static ParserObjects.Sequences;
+
+namespace ParserObjects.Tests.Examples.Parenthesis;
+
+public class ParenthesisValidatorTests
+{
+    [TestCase("", true)]
+    [TestCase("()", true)]
+    [TestCase("[]", true)]
+    [TestCase("{}", true)]
+    [TestCase("([{}])", true)]
+    [TestCase("()[]{}", true)]
+    [TestCase("()()()", true)]
+    [TestCase("((()))", true)]
+    [TestCase("[[[]]]", true)]
+    [TestCase("{{{}}}", true)]
+    [TestCase("({}[])", true)]
+    [TestCase("(}", false)]
+    [TestCase("(]", false)]
+    [TestCase("[}", false)]
+    public void Test(string input, bool expected)
+    {
+        var parser = ParenthesisGrammar.CreateParser();
+        var sequence = FromString(input);
+        var result = parser.Parse(sequence);
+        result.Success.Should().Be(expected);
+    }
+}
