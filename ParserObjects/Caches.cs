fabd37f8d3f7f1e32f31ccf2a4d625eb49227b52
Whiteknight <wknight8111@gmail.com>
2024-11-08 06:22:22 -0500

    SimpleDictionaryCache() type
    IParseState uses SimpleDictionaryCache as the default cache instead of NullCache. This means one extra allocation at the start of the parse operation, but otherwise saves us a lot of headache in Compose() and Deferred() parsers and any other parsers which use the cache to keep track of basic operating state

--- ParserObjects/Caches.cs
+++ ParserObjects/Caches.cs
@@ -9,6 +9,13 @@ namespace ParserObjects;
 public static class Caches
 {
     /// <summary>
+    /// Uses a simple Dictionary implementation as a cache.
+    /// </summary>
+    /// <returns></returns>
+    public static IResultsCache Dictionary()
+        => new SimpleDictionaryCache();
+
+    /// <summary>
     /// Create an in-memory cache using the IMemoryCache default instance.
     /// </summary>
     /// <returns></returns>
@@ -24,7 +31,9 @@ public static class Caches
         => new MemoryCacheResultsCache(memoryCache);
 
     /// <summary>
-    /// Create a null cache which does not cache any values.
+    /// Create a null cache which does not cache any values. WARNING: Caching is used to implement
+    /// behavior in several parser types. Using a null cache may lead to instable parses or
+    /// incorrect results.
     /// </summary>
     /// <returns></returns>
     public static IResultsCache NullCache()
