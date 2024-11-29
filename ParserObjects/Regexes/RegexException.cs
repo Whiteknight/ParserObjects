ad959fd990ff801b81d46baf22367aef98dbedc2
Whiteknight <wknight8111@gmail.com>
2024-11-29 14:11:20 -0500

    Address some SQ code smells

--- ParserObjects/Regexes/RegexException.cs
+++ ParserObjects/Regexes/RegexException.cs
@@ -5,9 +5,6 @@ namespace ParserObjects.Regexes;
 /// <summary>
 /// Exception thrown during Regex pattern parsing and regex engine execution.
 /// </summary>
-public class RegexException : Exception
+public class RegexException(string message) : Exception(message)
 {
-    public RegexException(string message) : base(message)
-    {
-    }
 }
