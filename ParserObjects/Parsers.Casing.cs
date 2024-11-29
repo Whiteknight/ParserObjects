649e5d4b1fd697890c12601ff974601b07e8ed5d
Whiteknight <wknight8111@gmail.com>
2024-11-29 12:03:35 -0500

    Misc cleanups

--- ParserObjects/Parsers.Casing.cs
+++ ParserObjects/Parsers.Casing.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using static ParserObjects.Parsers<char>;
 using static ParserObjects.Internal.ParserCache;
 
--- ParserObjects/Parsers.Lines.cs
+++ ParserObjects/Parsers.Lines.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ParserObjects.Internal.Parsers;
+﻿using ParserObjects.Internal.Parsers;
 using static ParserObjects.Parsers<char>;
 using static ParserObjects.Internal.ParserCache;
 
