6b0f6d785a01e3b725f10fe3ff4ce1ee8cf41c19
Whiteknight <wknight8111@gmail.com>
2024-11-29 12:28:57 -0500

    misc

--- ParserObjects/IParser.Utility.cs
+++ ParserObjects/IParser.Utility.cs
@@ -24,12 +24,11 @@ public static class ParserExtensions
     /// <param name="parser"></param>
     /// <returns></returns>
     public static string ToBnf(this IParser parser)
-    {
-        if (string.IsNullOrEmpty(parser.Name))
-            parser = parser.Named("(TARGET)");
-
-        return BnfStringifier.Instance.Stringify(parser);
-    }
+        => BnfStringifier.Instance.Stringify(
+            string.IsNullOrEmpty(parser.Name)
+                ? parser.Named("(TARGET)")
+                : parser
+        );
 
     /// <summary>
     /// Convert a parser and it's input sequence into a new sequence of parse result values.
