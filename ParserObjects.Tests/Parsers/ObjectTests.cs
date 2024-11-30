d21293fd5dee0475a778681d5a5fec735ed1380a
Whiteknight <wknight8111@gmail.com>
2024-11-30 10:32:00 -0500

    Tests for Stringify, Object() and ResultData struct

---
+++ ParserObjects.Tests/Parsers/ObjectTests.cs
@@ -0,0 +1,20 @@
+﻿using static ParserObjects.Parsers<char>;
+
+namespace ParserObjects.Tests.Parsers;
+
+public class ObjectTests
+{
+    [Test]
+    public void ToBnf()
+    {
+        var target = Object(Any()).Named("target");
+        target.ToBnf().Should().Contain("target := .");
+    }
+
+    [Test]
+    public void ToString_Test()
+    {
+        var target = Object(Any()).Named("target");
+        target.ToString().Should().NotBeNullOrEmpty();
+    }
+}
