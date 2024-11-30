bee6c25e1f055c2248ea10466cafb26c17526cf2
Whiteknight <wknight8111@gmail.com>
2024-11-30 10:51:33 -0500

    +tests for C methods and regex

---
+++ ParserObjects.Tests/Parsers/C/LongIntegerString.cs
@@ -0,0 +1,21 @@
+﻿using static ParserObjects.Parsers.C;
+
+namespace ParserObjects.Tests.Parsers.C;
+
+public class LongIntegerString
+{
+    [TestCase("0", "0")]
+    [TestCase("1", "1")]
+    [TestCase("123", "123")]
+    [TestCase("-123", "-123")]
+    [TestCase("0x0", "0x0")]
+    [TestCase("0x1234", "0x1234")]
+    [TestCase("0123", "0123")]
+    public void Parse_Test(string test, string value)
+    {
+        var parser = LongIntegerString();
+        var result = parser.Parse(test);
+        result.Success.Should().BeTrue();
+        result.Value.Should().Be(value);
+    }
+}
