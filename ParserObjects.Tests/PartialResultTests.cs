6f1e05bf856a5a10ebe8c8a1e7f3f9aa2e66142e
Whiteknight <wknight8111@gmail.com>
2024-11-30 14:34:06 -0500

    more tests

---
+++ ParserObjects.Tests/PartialResultTests.cs
@@ -0,0 +1,22 @@
+﻿using ParserObjects.Internal;
+
+namespace ParserObjects.Tests;
+
+public class PartialResultTests
+{
+    [Test]
+    public void Match_Success()
+    {
+        var target = new PartialResult<int>(5, 0);
+        var result = target.Match(x => x.ToString(), () => "FAIL");
+        result.Should().Be("5");
+    }
+
+    [Test]
+    public void Match_Error()
+    {
+        var target = new PartialResult<int>("ErrorMessage");
+        var result = target.Match(x => x.ToString(), () => "FAIL");
+        result.Should().Be("FAIL");
+    }
+}
